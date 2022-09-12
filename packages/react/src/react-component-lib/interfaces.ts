aed67ba6ec1908546ac633b11129d1c8d4b34875
Daniel Leroux <daniel.leroux@siemens.com>
2022-09-12 13:27:22 +0200

    refactor: add headers to autogenerated wrappers

--- packages/react/src/react-component-lib/interfaces.ts
+++ packages/react/src/react-component-lib/interfaces.ts
@@ -1,12 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2022 Siemens AG
- *
- * SPDX-License-Identifier: MIT
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 // General types important to applications using stencil built components
 export interface EventEmitter<T = any> {
   emit: (data?: T) => CustomEvent<T>;
