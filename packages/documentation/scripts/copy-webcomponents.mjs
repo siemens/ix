7d22bf74dd923cad818d9f018c4a2f69febd92ac
Daniel Leroux <daniel.leroux@siemens.com>
2022-10-04 14:11:23 +0200

    docs(examples): normalize example libs

--- packages/documentation/scripts/copy-webcomponents.mjs
+++ packages/documentation/scripts/copy-webcomponents.mjs
@@ -10,8 +10,6 @@
 import fsExtra from 'fs-extra';
 import path from 'path';
 
-import { createRequire } from 'module';
-
 const __dirname = path.resolve();
 const libDestPath = path.join(
   __dirname,
@@ -20,55 +18,34 @@ const libDestPath = path.join(
   'lib'
 );
 
-const ix_path = path.join(__dirname, '..', 'core');
-const ix_dest_path = path.join(libDestPath, 'ix');
-
-const ix_brand_theme_path = path.join(
-  __dirname,
-  '../../',
-  'node_modules',
-  '@siemens/ix-brand-theme'
-);
-const ix_brand_theme_dest_path = path.join(libDestPath, 'ix-brand-theme');
-
-const ix_aggrid_path = path.join(__dirname, '..', 'aggrid');
-const ix_aggrid_dest_path = path.join(libDestPath, 'ix-aggrid');
-
-const icon_path = path.join(
-  __dirname,
-  '../../',
-  'node_modules',
-  '@siemens/ix-icons'
-);
-const icon_dest_path = path.join(libDestPath, 'ix-icons');
-
-const { resolve } = createRequire(import.meta.url);
+const node_modules = path.join(__dirname, '../../', 'node_modules');
+const ix_brand_theme_path = path.join(node_modules, '@siemens/ix-brand-theme');
 
 async function loadLib(libName) {
-  const distName = '/dist';
-  const libPath = resolve(libName).replace(/\\/g, '/');
-  const distPath = libPath.substring(
-    0,
-    libPath.indexOf(distName) + distName.length
+  const libPath = path.join(node_modules, libName); //require.resolve(libName).replace(/\\/g, '/');
+
+  const pkg = JSON.parse(fsExtra.readFileSync(`${libPath}/package.json`));
+
+  console.log(pkg);
+
+  return Promise.all(
+    pkg.files.map((file) => {
+      return fsExtra.copy(
+        `${libPath}/${file}`,
+        path.join(libDestPath, libName, file)
+      );
+    })
   );
-
-  return fsExtra.copy(distPath, path.join(libDestPath, libName));
-}
-
-function filter(fileName) {
-  return !fileName.includes('node_modules');
 }
 
 (async () => {
   console.log('Start copy');
   await Promise.all([
-    fsExtra.copy(ix_path, ix_dest_path, { filter }),
-    fsExtra.copy(ix_aggrid_path, ix_aggrid_dest_path, { filter }),
-    fsExtra.copy(icon_path, icon_dest_path),
+    loadLib('@siemens/ix'),
+    loadLib('@siemens/ix-icons'),
+    loadLib('@siemens/ix-aggrid'),
     fsExtra.pathExistsSync(ix_brand_theme_path)
-      ? fsExtra.copy(ix_brand_theme_path, ix_brand_theme_dest_path, {
-          filter: (path) => !path.includes('ix-brand-theme/node_modules'),
-        })
+      ? loadLib('@siemens/ix-brand-theme')
       : Promise.resolve(),
     loadLib('@siemens/ix-echarts'),
   ]);
