a784aa530c426d00e8936ddcc5b1f177af1eb341
goncalosard <innsiiiider@hotmail.com>
2022-11-14 17:19:39 +0000

    fix(core): breacrumbs visual regretion testcase

--- packages/core/src/components/breadcrumb/test/breadcrumb.e2e.ts
+++ packages/core/src/components/breadcrumb/test/breadcrumb.e2e.ts
@@ -15,4 +15,17 @@ regressionTest.describe('breadcrumb', () => {
     await page.goto(`breadcrumb/test/basic`);
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
   });
+
+  regressionTest('lazyLoaded', async ({ page }) => {
+    await page.goto(`breadcrumb/test/lazyLoaded`);
+    await page.locator('text=Item3').click();
+    // const dropdown = await page.$('#breadcrumb');
+
+    // await page.locator('#here').selectOption({label: 'Next Item 1'})
+    // await page.pause;
+
+    // await dropdown.selectOption({label: 'Next Item 4'})
+
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
+  });
 });
