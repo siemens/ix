46c736f0122bb1afad2205ae8322d2afb70423c6
Daniel Leroux <daniel.leroux@siemens.com>
2022-12-13 08:34:16 +0100

    test(aggrid): add visual regression tests for aggrid

--- packages/aggrid/src/utils/test/index.ts
+++ packages/aggrid/src/utils/test/index.ts
@@ -7,6 +7,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export function format(first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
-}
+export * from './page';
