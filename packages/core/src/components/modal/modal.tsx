e94ee314cda39de7b71a8e0d3e731f87379411c8
Lukas Maurer <lukas.maurer@siemens.com>
2023-01-11 11:20:05 +0100

    fix(core/modal): make sure background static only dismisses modal on actual backdrop

--- packages/core/src/components/modal/modal.tsx
+++ packages/core/src/components/modal/modal.tsx
@@ -167,9 +167,19 @@ export class Modal {
     });
   }
 
+  private onBackdropClick(event: Event) {
+    const target = event.target as Element;
+    if (target.classList.contains('backdrop')) {
+      this.dismiss(event);
+    }
+  }
+
   componentDidLoad() {
     if (this.backdrop === 'static') {
-      this.modalBackdrop.addEventListener('click', this.dismiss.bind(this));
+      this.modalBackdrop.addEventListener(
+        'click',
+        this.onBackdropClick.bind(this)
+      );
     }
 
     if (this.backdropClass) {
