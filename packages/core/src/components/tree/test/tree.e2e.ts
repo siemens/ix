0f111d11b1e70e64dfc3855c07edcd4f5c375842
Daniel Leroux <daniel.leroux@siemens.com>
2023-01-19 13:02:25 +0100

    test: add tree visual regression

--- packages/core/src/components/tree/test/tree.e2e.ts
+++ packages/core/src/components/tree/test/tree.e2e.ts
@@ -11,10 +11,62 @@ import { expect } from '@playwright/test';
 import { regressionTest } from '@utils/test';
 
 regressionTest.describe('tree', () => {
-  regressionTest('basic', async ({ page }) => {
+  regressionTest('should append item to tree', async ({ page }) => {
     await page.goto('tree/test/basic');
-    // await page.waitForSelector('ix-tree-item');
-    await page.waitForTimeout(2000);
+
+    const treeViewportHandle = await page.waitForSelector('ix-tree');
+
+    await page.evaluate((treeViewport) => {
+      treeViewport.scrollTop = 32 * 999;
+    }, treeViewportHandle);
+
+    await page.evaluate((tree) => {
+      const model = tree.model;
+
+      model['last-child'] = {
+        id: 'last-child',
+        children: [],
+        hasChildren: false,
+        data: { name: 'last-child' },
+      };
+
+      model['sample'].children.push('last-child');
+
+      tree.model = { ...model };
+      tree.scrollTop = 32 * 999;
+    }, treeViewportHandle);
+
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
+  });
+
+  regressionTest('should keep scroll state', async ({ page }) => {
+    await page.goto('tree/test/basic');
+
+    const treeViewportHandle = await page.waitForSelector('ix-tree');
+
+    await page.evaluate((treeViewport) => {
+      treeViewport.scrollTop = 32 * 50;
+    }, treeViewportHandle);
+
+    await page.evaluate((tree) => {
+      const model = tree.model;
+
+      model['insert-below-50'] = {
+        id: 'insert-below-50',
+        children: [],
+        hasChildren: false,
+        data: { name: 'insert-below-50' },
+      };
+
+      const indexOfItem50 = tree.model.sample.children.findIndex(
+        (id) => id === 'sample-child-50'
+      );
+
+      model['sample'].children.splice(indexOfItem50 + 1, 0, 'insert-below-50');
+
+      tree.model = { ...model };
+    }, treeViewportHandle);
+
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
   });
 });
