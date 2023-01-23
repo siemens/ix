b41735ddec83f3838865061f7e58e8c53d0b6328
goncalosard <innsiiiider@hotmail.com>
2023-01-23 15:54:35 +0000

    fix(core): event-list hover

--- packages/core/src/components/my-component/my-component.tsx
+++ packages/core/src/components/my-component/my-component.tsx
@@ -16,6 +16,17 @@ import { Component, h, Host } from '@stencil/core';
 })
 export class MyComponent {
   render() {
-    return <Host></Host>;
+    return (
+      <Host>
+        <ix-event-list>
+          <ix-event-list-item color="color-primary">Text 1</ix-event-list-item>
+          <ix-event-list-item color="color-primary" selected>
+            Text 2
+          </ix-event-list-item>
+          <ix-event-list-item color="color-primary">Text 3</ix-event-list-item>
+          <ix-event-list-item color="color-primary">Text 4</ix-event-list-item>
+        </ix-event-list>
+      </Host>
+    );
   }
 }
