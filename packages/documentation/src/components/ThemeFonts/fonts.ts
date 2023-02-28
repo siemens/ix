e8fd5ed359d6b5b8cb3fd97d0018317e14df380f
Daniel Leroux <daniel.leroux@siemens.com>
2023-02-28 15:39:20 +0100

    docs: refactor styling (#406)

---
+++ packages/documentation/src/components/ThemeFonts/fonts.ts
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const themeFonts = [
+  '--theme-text-h2',
+  '--theme-text-xl',
+  '--theme-text-l-title',
+  '--theme-text-l-title-single',
+  '--theme-text-l',
+  '--theme-text-l-single',
+  '--theme-text-default-title',
+  '--theme-text-default-title-single',
+  '--theme-text-default',
+  '--theme-text-default-single',
+  '--theme-text-caption',
+  '--theme-text-caption-single',
+  '--theme-text-s',
+  '--theme-text-s-single',
+  '--theme-text-xs',
+];
