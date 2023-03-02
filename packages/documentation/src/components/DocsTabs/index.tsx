6a5089612de8645e4c5b1611ad9d5c27bfbe9f64
Daniel Leroux <daniel.leroux@siemens.com>
2023-03-02 12:08:11 +0100

    feat: replace event renderer with new ApiTable

--- packages/documentation/src/components/DocsTabs/index.tsx
+++ packages/documentation/src/components/DocsTabs/index.tsx
@@ -61,9 +61,9 @@ const DocsTabs = (props: DocsTabsProps) => {
           onClick={() => onTabChange('styleguide')}
         >
           <svg
-            width="25"
+            width="24"
             height="24"
-            viewBox="0 0 25 24"
+            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
@@ -79,9 +79,9 @@ const DocsTabs = (props: DocsTabsProps) => {
           onClick={() => onTabChange('code')}
         >
           <svg
-            width="25"
+            width="24"
             height="24"
-            viewBox="0 0 25 24"
+            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
