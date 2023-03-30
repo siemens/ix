60476fb9e13fb96b1c08eca95e1f213baf53f55a
Lukas Maurer <lukas.maurer@siemens.com>
2023-03-30 11:32:27 +0200

    fix(core/blind): adjust padding and update docs

--- packages/vue-test-app/src/preview-examples/blind-header-actions.vue
+++ packages/vue-test-app/src/preview-examples/blind-header-actions.vue
@@ -1,18 +1,15 @@
-/*
- * SPDX-FileCopyrightText: 2023 Siemens AG
- *
- * SPDX-License-Identifier: MIT
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 <script setup lang="ts">
 import { IxBlind, IxDropdown, IxIconButton } from '@siemens/ix-vue';
 </script>
 
 <template>
   <IxBlind label="Example">
-    <IxIconButton slot="header-actions" id="context-menu" icon="context-menu"></IxIconButton>
+    <IxIconButton
+      slot="header-actions"
+      id="context-menu"
+      ghost
+      icon="context-menu"
+    ></IxIconButton>
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
     eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
     voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
