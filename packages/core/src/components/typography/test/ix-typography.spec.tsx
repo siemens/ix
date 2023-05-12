2f592f48feebbf2d097f08becdaa5f080252b240
Daniel Leroux <daniel.leroux@siemens.com>
2023-05-12 11:01:26 +0200

    feat(core/typography): add color property

--- packages/core/src/components/typography/test/ix-typography.spec.tsx
+++ packages/core/src/components/typography/test/ix-typography.spec.tsx
@@ -10,7 +10,41 @@ describe('ix-typography', () => {
     expect(page.root).toEqualHtml(`
       <ix-typography>
         <mock:shadow-root>
-        <div class="text-default">
+        <div class="text-default" style="color: inherit;">
+          <slot></slot>
+        </div>
+        </mock:shadow-root>
+        Example content
+      </ix-typography>
+    `);
+  });
+
+  it('should render color', async () => {
+    const page = await newSpecPage({
+      components: [IxTypography],
+      html: `<ix-typography color="soft">Example content</ix-typography>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <ix-typography color="soft">
+        <mock:shadow-root>
+        <div class="text-default" style="color: var(--theme-color-soft-text);">
+          <slot></slot>
+        </div>
+        </mock:shadow-root>
+        Example content
+      </ix-typography>
+    `);
+  });
+
+  it('should render font style', async () => {
+    const page = await newSpecPage({
+      components: [IxTypography],
+      html: `<ix-typography variant="h2">Example content</ix-typography>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <ix-typography variant="h2">
+        <mock:shadow-root>
+        <div class="text-h2" style="color: inherit;">
           <slot></slot>
         </div>
         </mock:shadow-root>
