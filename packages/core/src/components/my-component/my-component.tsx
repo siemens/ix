1f1fa15546db98b5ab16159152806119c99eff80
Daniel Leroux <daniel.leroux@siemens.com>
2023-05-24 10:49:17 +0200

    feat: change box-sizing

--- packages/core/src/components/my-component/my-component.tsx
+++ packages/core/src/components/my-component/my-component.tsx
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Component, h, Host, State } from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -15,34 +15,7 @@ import { Component, h, Host, State } from '@stencil/core';
   scoped: true,
 })
 export class MyComponent {
-  changeTab = (tabId: number) => (this.selectedTab = tabId);
-
-  @State() selectedTab = 0;
-
   render() {
-    return (
-      <Host>
-        <ix-tabs rounded>
-          <ix-tab-item>
-            <ix-icon name="success"></ix-icon>
-          </ix-tab-item>
-          <ix-tab-item counter={200}>
-            <ix-icon name="tree"></ix-icon>
-          </ix-tab-item>
-          <ix-tab-item>
-            <ix-icon name="maintenance"></ix-icon>
-          </ix-tab-item>
-          <ix-tab-item disabled counter={24}>
-            <ix-icon name="sound-loud"></ix-icon>
-          </ix-tab-item>
-          <ix-tab-item>
-            <ix-icon name="hierarchy"></ix-icon>
-          </ix-tab-item>
-          <ix-tab-item disabled>
-            <ix-icon name="calendar-settings"></ix-icon>
-          </ix-tab-item>
-        </ix-tabs>
-      </Host>
-    );
+    return <Host></Host>;
   }
 }
