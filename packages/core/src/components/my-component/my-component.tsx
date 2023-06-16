1bf59091f0e59500940583af4eca0243252eeeda
Daniel Leroux <daniel.leroux@siemens.com>
2023-06-16 08:04:49 +0200

    chore: cleanup

--- packages/core/src/components/my-component/my-component.tsx
+++ packages/core/src/components/my-component/my-component.tsx
@@ -7,6 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { Component, h, Host } from '@stencil/core';
+
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.scss',
