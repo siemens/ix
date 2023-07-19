3c1eccfc6dedcf3fb5294fa2defa1c444cf973b4
Daniel Leroux <daniel.leroux@siemens.com>
2023-07-19 08:46:04 +0200

    fix(core): change variant values to lower case

--- packages/core/src/components/my-component/my-component.tsx
+++ packages/core/src/components/my-component/my-component.tsx
@@ -15,6 +15,55 @@ import { Component, h, Host } from '@stencil/core';
 })
 export class MyComponent {
   render() {
-    return <Host></Host>;
+    return (
+      <Host>
+        <ix-dropdown-button label="Test">
+          <ix-dropdown-header label="Category"></ix-dropdown-header>
+          <ix-dropdown-item label="Test"></ix-dropdown-item>
+
+          <ix-divider></ix-divider>
+
+          <ix-dropdown-item>Test</ix-dropdown-item>
+          <ix-dropdown-item
+            icon="bulb"
+            label="Test"
+            disabled
+          ></ix-dropdown-item>
+          <ix-dropdown-item checked icon="bulb">
+            Test
+          </ix-dropdown-item>
+          <ix-dropdown-item
+            style={{
+              width: '10rem',
+            }}
+          >
+            Test Test Test Test Test TestTest Test Test TestTest Test Test
+            TestTest Test Test Test
+          </ix-dropdown-item>
+        </ix-dropdown-button>
+
+        <ix-dropdown-button label="Test">
+          <ix-dropdown-item icon="bulb"></ix-dropdown-item>
+          <ix-dropdown-item icon="bulb" disabled></ix-dropdown-item>
+          <hr class={'dropdown-divider'}></hr>
+          <ix-dropdown-item icon="bulb"></ix-dropdown-item>
+        </ix-dropdown-button>
+
+        <ix-button id="iconTriggerId">Open</ix-button>
+        <ix-dropdown trigger="iconTriggerId">
+          <ix-dropdown-item
+            id="submenuTrigger"
+            label="Submenu"
+          ></ix-dropdown-item>
+          <ix-dropdown-item icon="star" label="Item 1"></ix-dropdown-item>
+          <ix-dropdown-item icon="document" label="Item 2"></ix-dropdown-item>
+          <ix-dropdown-item icon="bulb" label="Item 3"></ix-dropdown-item>
+        </ix-dropdown>
+        <ix-dropdown trigger="submenuTrigger" placement="right-start">
+          <ix-dropdown-item icon="star" label="Item 1"></ix-dropdown-item>
+          <ix-dropdown-item icon="document" label="Item 2"></ix-dropdown-item>
+        </ix-dropdown>
+      </Host>
+    );
   }
 }
