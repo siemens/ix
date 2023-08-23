c0e92c89f79e03a42d47024cf9489556b2a01e3e
Daniel Leroux <daniel.leroux@siemens.com>
2023-08-23 12:28:27 +0200

    docs: add react examples

---
+++ packages/react-test-app/src/preview-examples/grid-size.tsx
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { IxCol, IxGrid, IxRow } from '@siemens/ix-react';
+import React from 'react';
+// Example styling for documentation
+import './grid.css';
+
+export default () => {
+  return (
+    <>
+      <h4>Column 6 takes all space until reached column 12</h4>
+      <IxGrid>
+        <IxRow>
+          <IxCol>1</IxCol>
+          <IxCol>2</IxCol>
+          <IxCol>3</IxCol>
+          <IxCol>4</IxCol>
+          <IxCol>5</IxCol>
+          <IxCol size="6">6</IxCol>
+        </IxRow>
+      </IxGrid>
+
+      <h4>Stack on smaller screens as md</h4>
+      <IxGrid>
+        <IxRow>
+          <IxCol size="12" size-md="3">
+            1
+          </IxCol>
+          <IxCol size="12" size-md="3">
+            2
+          </IxCol>
+          <IxCol size="12" size-md="3">
+            3
+          </IxCol>
+          <IxCol size="12" size-md="3">
+            4
+          </IxCol>
+        </IxRow>
+      </IxGrid>
+    </>
+  );
+};
