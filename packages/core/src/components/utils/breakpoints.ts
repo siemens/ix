00b1ee194e6f7e0cde310a5d9c5a1923df82252d
Daniel Leroux <daniel.leroux@siemens.com>
2023-08-25 15:11:00 +0200

    refactor: remove mixins

--- packages/core/src/components/utils/breakpoints.ts
+++ packages/core/src/components/utils/breakpoints.ts
@@ -6,6 +6,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+// Implementation of scss breakpoints mixins/_break-points.scss
 const mediaQueries = {
   sm: '(min-width: 36em)',
   md: '(min-width: 48em)',
