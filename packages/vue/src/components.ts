8f2900b9808cc487a578c9ecfcfb39cf25060253
lzeiml <lukas.zeiml@gmail.com>
2023-09-18 15:28:24 +0200

    fix: build

--- packages/vue/src/components.ts
+++ packages/vue/src/components.ts
@@ -354,8 +354,7 @@ export const IxGroupItem = /*@__PURE__*/ defineContainer<JSX.IxGroupItem>('ix-gr
 export const IxIcon = /*@__PURE__*/ defineContainer<JSX.IxIcon>('ix-icon', undefined, [
   'size',
   'color',
-  'name',
-  'src'
+  'name'
 ]);
 
 
