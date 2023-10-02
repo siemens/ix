291a3a4432786a7c8a1e5224bc96f60986fd00c2
lzeiml <lukas.zeiml@gmail.com>
2023-10-02 16:13:11 +0200

    test(core/date-input): add date-input tests

--- packages/core/src/components/date-input/test/date-input.ct.ts
+++ packages/core/src/components/date-input/test/date-input.ct.ts
@@ -1,10 +1,60 @@
-import { expect } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import { test } from '@utils/test';
 
 const DATE_INPUT_SELECTOR = 'ix-date-input';
+const getInputObj = (page: Page) => {
+  return page.$eval(DATE_INPUT_SELECTOR, (el: HTMLIxDateInputElement) =>
+    el.getCurrentInput()
+  );
+};
 
-test('renders', async ({ mount, page }) => {
-  await mount(`<${DATE_INPUT_SELECTOR}></${DATE_INPUT_SELECTOR}>`);
-  const dateInput = page.locator(DATE_INPUT_SELECTOR);
-  await expect(dateInput).toHaveClass(/hydrated/);
+test.describe('date input tests single', () => {
+  test.beforeEach(async ({ mount }) => {
+    await mount(`<ix-date-input range="false"></ix-date-input>`);
+  });
+
+  test('renders', async ({ page }) => {
+    const dateInput = page.locator(DATE_INPUT_SELECTOR);
+    await expect(dateInput).toHaveClass(/hydrated/);
+  });
+
+  test('no initial value', async ({ page }) => {
+    expect(await getInputObj(page)).toEqual({
+      from: undefined,
+      to: undefined,
+    });
+  });
+
+  test('entered date is selected', async ({ page }) => {
+    const testDate = '2023/02/15';
+
+    await page.getByPlaceholder('YYYY/MM/DD').click();
+    await page.getByPlaceholder('YYYY/MM/DD').fill(testDate);
+
+    const month = page
+      .locator('ix-button')
+      .filter({ hasText: 'February 2023' });
+    const day = page.getByText('15');
+
+    await expect(month).toHaveClass(/hydrated/);
+    await expect(day).toHaveClass(/selected/);
+    expect(await getInputObj(page)).toEqual({
+      from: testDate,
+      to: undefined,
+    });
+  });
+
+  test('selected date is entered in input', async ({ page }) => {
+    page.$eval(DATE_INPUT_SELECTOR, (el: HTMLIxDateInputElement) => {
+      el.from = '2023/01/01';
+    });
+
+    await page.getByText('2023/01/01').click();
+    await page.getByText('13').click();
+
+    expect(await getInputObj(page)).toEqual({
+      from: '2023/01/13',
+      to: undefined,
+    });
+  });
 });
