6166c7388bf14b79214dfdb524d5df58cce359d6
Daniel Leroux <daniel.leroux@siemens.com>
2023-10-20 14:38:32 +0200

    chore: change header

--- packages/documentation/scripts/generate-markdown.ts
+++ packages/documentation/scripts/generate-markdown.ts
@@ -1,5 +1,10 @@
 /*
- * COPYRIGHT (c) Siemens AG 2018-2023 ALL RIGHTS RESERVED.
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 import fs from 'fs-extra';
 import { Listr } from 'listr2';
