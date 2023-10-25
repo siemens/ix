8569151fd775510236ec56f261a6ece544cfa672
Daniel Leroux <daniel.leroux@siemens.com>
2023-10-25 08:04:40 +0200

    fix(core/blind): remove double hover effect

---
+++ packages/core/src/components/blind/test/blind.ct.ts
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { expect } from '@playwright/test';
+import { test } from '@utils/test';
+
+test('renders', async ({ mount, page }) => {
+  await mount(`<ix-blind label="Example label">Some content</ix-blind>`);
+  const blindElement = page.locator('ix-blind');
+  await expect(blindElement).toHaveClass(/hydrated/);
+});
