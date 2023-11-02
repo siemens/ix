d7f12928ebe9146c385021eccfd09aba4efdb2ad
matthiashader <144090716+matthiashader@users.noreply.github.com>
2023-11-02 09:54:24 +0100

    docs(core/basic-navigation): missing files from yarn build

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -147,6 +147,7 @@ export namespace Components {
         "applicationName": string;
         /**
           * Supported layouts
+          * @example ['sm', 'md']
          */
         "breakpoints": Breakpoint[];
         /**
@@ -3183,6 +3184,7 @@ declare namespace LocalJSX {
         "applicationName"?: string;
         /**
           * Supported layouts
+          * @example ['sm', 'md']
          */
         "breakpoints"?: Breakpoint[];
         /**
