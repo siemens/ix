e972105dcac78e8815e12fe3d65e6d9b670346ab
Daniel Leroux <leroux.daniel@googlemail.com>
2023-11-06 21:31:23 +0100

    ci: use playwright image (#12)
    * ci: use playwright image
    
    * ci: test sharding
    
    * ci: split testing
    
    * ci: remove reporter from scripts
    
    * ci: debug
    
    * fix: fix
    
    * fix: revert
    
    * ci: matrix
    
    * ci: add aggrid
    
    * ci: cleanup

--- packages/core/playwright.config.ts
+++ packages/core/playwright.config.ts
@@ -62,7 +62,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: 10,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
