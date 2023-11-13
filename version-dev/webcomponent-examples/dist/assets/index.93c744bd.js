20982e37282551868521260b86fba7b452247a6c
danielleroux <danielleroux@users.noreply.github.com>
2023-11-13 10:46:06 +0000

    Deploying to gh-pages from @ siemens/ix@03b895b6836e9df620133c08a040dd83907d8055 🚀

--- version-dev/webcomponent-examples/dist/assets/index.93c744bd.js
+++
@@ -1,5 +0,0 @@
-import { a as applyPolyfills, d as defineCustomElements } from "./index.8724426d.js";
-(async () => {
-  await applyPolyfills();
-  await defineCustomElements();
-})();
---
+++ version-dev/webcomponent-examples/dist/assets/ix-menu-settings-item.entry.4599e1d2.js
@@ -0,0 +1,15 @@
+import { r as registerInstance, h, H as Host } from "./index.d3e048f1.js";
+const menuSettingsItemCss = ":host{display:block}";
+const MenuSettingsItem = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+    this.label = void 0;
+  }
+  render() {
+    return h(Host, null, h("slot", null));
+  }
+};
+MenuSettingsItem.style = menuSettingsItemCss;
+export {
+  MenuSettingsItem as ix_menu_settings_item
+};
