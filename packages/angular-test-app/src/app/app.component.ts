ba5f7d7896f79ba1c46accb52b1a83dd8c321389
Julian Lamplmair <julian.lamplmair.ext@siemens.com>
2023-11-15 14:08:30 +0100

    fix: update angular example env

--- packages/angular-test-app/src/app/app.component.ts
+++ packages/angular-test-app/src/app/app.component.ts
@@ -12,7 +12,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-test-app';
