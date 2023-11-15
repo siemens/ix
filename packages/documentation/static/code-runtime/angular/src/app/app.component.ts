9883440e9483d10b622a2ce49571d5fe562c4664
Julian Lamplmair <julian.lamplmair.ext@siemens.com>
2023-11-15 14:04:03 +0100

    fix: angular env

--- packages/documentation/static/code-runtime/angular/src/app/app.component.ts
+++ packages/documentation/static/code-runtime/angular/src/app/app.component.ts
@@ -12,6 +12,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
