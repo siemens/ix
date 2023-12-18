3fd1631812abc086e3d464b6413376e01bb3b231
Daniel Leroux <daniel.leroux@siemens.com>
2023-12-18 07:15:53 +0100

    fix: build

--- packages/react-test-app/src/App.tsx
+++ packages/react-test-app/src/App.tsx
@@ -6,62 +6,21 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { AppSwitchConfiguration } from '@siemens/ix';
-import {
-  IxApplication,
-  IxApplicationContext,
-  IxApplicationHeader,
-  IxAvatar,
-  IxMenu,
-  IxMenuItem,
-} from '@siemens/ix-react';
-import React, { useEffect, useState } from 'react';
+import { IxButton } from '@siemens/ix-react';
+import React, { useState } from 'react';
 
-const apps = [
-  {
-    id: 'app-1',
-    name: 'Example App 1',
-    iconSrc: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
-    url: 'https://www.google.de',
-    description: 'Test',
-    target: '_self',
-  },
-];
+function ToggleMe(props: { toggle: boolean }) {
+  return <IxButton>{props.toggle ? 'Test 1' : 'Test 2'}</IxButton>;
+}
 
 function App() {
-  const [toggle, setToggle] = useState<AppSwitchConfiguration>({
-    textAppSwitch: 'Aendere die Applikation',
-    currentAppId: 'app-2',
-    apps: [],
-  });
-
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('timeout');
-      setToggle({
-        ...toggle,
-        apps: apps,
-      });
-    }, 5000);
-  }, []);
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <>
-      <IxApplicationContext>
-        <IxApplication appSwitchConfig={toggle}>
-          <IxApplicationHeader name="Test App">
-            <IxAvatar></IxAvatar>
-          </IxApplicationHeader>
-          <IxMenu>
-            <IxMenuItem>Test</IxMenuItem>
-            <IxMenuItem>Test</IxMenuItem>
-            <IxMenuItem>Test</IxMenuItem>
-            <IxMenuItem>Test</IxMenuItem>
-            <IxMenuItem>Test</IxMenuItem>
-          </IxMenu>
-        </IxApplication>
-      </IxApplicationContext>
-    </>
+    <div>
+      <IxButton onClick={() => setToggle(!toggle)}>Toggle!</IxButton>
+      <ToggleMe toggle={toggle} />
+    </div>
   );
 }
 
