910316e6910246b436451da91f59b70065826d5d
jul-lam <julian.lamplmair.ext@siemens.com>
2023-12-19 14:54:37 +0100

    fix(react): update jest implementation

--- packages/react/jest-preprocess.js
+++ packages/react/jest-preprocess.js
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const babelOptions = {
-  presets: ['@babel/preset-typescript'],
-  plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
 };
 
 module.exports = require('babel-jest').default.createTransformer(babelOptions);
