345bd49eb0c72e19cbe709a05fae18aed5b45393
jul-lam <julian.lamplmair.ext@siemens.com>
2023-12-20 12:54:45 +0100

    feat(react): add jest tests for known issues

---
+++ packages/react/src/tests/undefined-error/undefined-error.spec.tsx
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+import Content from './undefined-error';
+
+describe(`undefined-error`, () => {
+  it(`basic`, () => {
+    const { getByText } = render(<Content />);
+
+    const button = getByText('Open');
+    button.click();
+  });
+});
