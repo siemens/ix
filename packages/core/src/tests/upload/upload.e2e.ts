9198d31be2c13a09c67e4094a10668e9c710304f
Daniel Leroux <daniel.leroux@siemens.com>
2024-01-08 07:00:56 +0100

    fix(core/upload): replace class icons with ix-icon (#1022)

--- packages/core/src/tests/upload/upload.e2e.ts
+++ packages/core/src/tests/upload/upload.e2e.ts
@@ -8,11 +8,49 @@
  */
 
 import { expect } from '@playwright/test';
+import { UploadFileState } from '@siemens/ix';
 import { regressionTest } from '@utils/test';
 
 regressionTest.describe('upload', () => {
   regressionTest('basic', async ({ page }) => {
     await page.goto('upload/basic');
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
+    await expect(page).toHaveScreenshot();
+  });
+
+  regressionTest('States', async ({ page }) => {
+    await page.goto('upload/states');
+
+    const uploads = await page.locator('ix-upload').all();
+
+    await Promise.all(
+      uploads.map(async (locator, index) => {
+        let state: UploadFileState;
+        switch (index) {
+          case 0:
+            state = UploadFileState.LOADING;
+            break;
+          case 1:
+            state = UploadFileState.SELECT_FILE;
+            break;
+          case 2:
+            state = UploadFileState.UPLOAD_FAILED;
+            break;
+          case 3:
+            state = UploadFileState.UPLOAD_SUCCESSED;
+            break;
+          default:
+            console.error('Invalid enum value');
+        }
+
+        await locator.evaluate(
+          (elm: HTMLIxUploadElement, state: UploadFileState) => {
+            elm.state = state;
+          },
+          state
+        );
+      })
+    );
+
+    await expect(page).toHaveScreenshot();
   });
 });
