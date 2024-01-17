72483de563754f08f91985af85c55eaedfb560db
Daniel Leroux <daniel.leroux@siemens.com>
2024-01-17 10:22:45 +0100

    fix: example

--- packages/core/src/components/my-component/my-component.tsx
+++ packages/core/src/components/my-component/my-component.tsx
@@ -17,49 +17,71 @@ export class MyComponent {
   render() {
     return (
       <Host>
-        <ix-dropdown-button label="xxx">
-          <ix-dropdown-item>Test 1</ix-dropdown-item>
-          <ix-dropdown-item>Test 2</ix-dropdown-item>
-          <ix-dropdown-item id="Menu1">Test 3</ix-dropdown-item>
-          <ix-dropdown-item id="Menu2">Test 4</ix-dropdown-item>
-        </ix-dropdown-button>
+        <ix-application>
+          <ix-application-header>
+            <ix-dropdown-button label="xxx">
+              <ix-dropdown-item>Test 1</ix-dropdown-item>
+              <ix-dropdown-item>Test 2</ix-dropdown-item>
+              <ix-dropdown-item>Test 3</ix-dropdown-item>
+              <ix-dropdown-item>Test 4</ix-dropdown-item>
+            </ix-dropdown-button>
 
-        <ix-dropdown trigger={'Menu1'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-avatar>
+              <ix-dropdown-item>1</ix-dropdown-item>
+              <ix-dropdown-item id="avatarsub">2</ix-dropdown-item>
+            </ix-avatar>
+          </ix-application-header>
+          <ix-content>
+            <ix-dropdown trigger={'avatarsub'}>
+              <ix-dropdown-item>ItemAvatar1</ix-dropdown-item>
+              <ix-dropdown-item>ItemAvatar2</ix-dropdown-item>
+            </ix-dropdown>
 
-        <ix-dropdown trigger={'Menu2'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item id="Menu2_SubMenu21">Item</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-dropdown-button label="xxx">
+              <ix-dropdown-item>Test 1</ix-dropdown-item>
+              <ix-dropdown-item>Test 2</ix-dropdown-item>
+              <ix-dropdown-item id="Menu1">Test 3</ix-dropdown-item>
+              <ix-dropdown-item id="Menu2">Test 4</ix-dropdown-item>
+            </ix-dropdown-button>
 
-        <ix-dropdown trigger={'Menu2_SubMenu21'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item>Item deep</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-dropdown trigger={'Menu1'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+            </ix-dropdown>
 
-        <ix-dropdown-button label="xxx">
-          <ix-dropdown-item>Test 1</ix-dropdown-item>
-          <ix-dropdown-item>Test 2</ix-dropdown-item>
-          <ix-dropdown-item id="XXMenu1">Test 3</ix-dropdown-item>
-          <ix-dropdown-item id="XXMenu2">Test 4</ix-dropdown-item>
-        </ix-dropdown-button>
+            <ix-dropdown trigger={'Menu2'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item id="Menu2_SubMenu21">Item</ix-dropdown-item>
+            </ix-dropdown>
 
-        <ix-dropdown trigger={'XXMenu1'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-dropdown trigger={'Menu2_SubMenu21'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item>Item deep</ix-dropdown-item>
+            </ix-dropdown>
 
-        <ix-dropdown trigger={'XXMenu2'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item id="XXMenu2_SubXXMenu21">Item</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-dropdown-button label="xxx">
+              <ix-dropdown-item>Test 1</ix-dropdown-item>
+              <ix-dropdown-item>Test 2</ix-dropdown-item>
+              <ix-dropdown-item id="XXMenu1">Test 3</ix-dropdown-item>
+              <ix-dropdown-item id="XXMenu2">Test 4</ix-dropdown-item>
+            </ix-dropdown-button>
 
-        <ix-dropdown trigger={'XXMenu2_SubXXMenu21'}>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-          <ix-dropdown-item>Item</ix-dropdown-item>
-        </ix-dropdown>
+            <ix-dropdown trigger={'XXMenu1'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+            </ix-dropdown>
+
+            <ix-dropdown trigger={'XXMenu2'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item id="XXMenu2_SubXXMenu21">Item</ix-dropdown-item>
+            </ix-dropdown>
+
+            <ix-dropdown trigger={'XXMenu2_SubXXMenu21'}>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+              <ix-dropdown-item>Item</ix-dropdown-item>
+            </ix-dropdown>
+          </ix-content>
+        </ix-application>
       </Host>
     );
   }
