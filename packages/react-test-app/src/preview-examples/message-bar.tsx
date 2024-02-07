5b5bd14b0263cf372c140ea194e431e712cedfa6
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-02-07 07:22:02 +0100

    fix(core/message-bar): adjusted preview-examples and test

--- packages/react-test-app/src/preview-examples/message-bar.tsx
+++ packages/react-test-app/src/preview-examples/message-bar.tsx
@@ -13,13 +13,17 @@ import React from 'react';
 export default () => {
   return (
     <>
-      <IxMessageBar>Message text</IxMessageBar>
-      <IxMessageBar type="warning">Message text</IxMessageBar>
-      <IxMessageBar type="danger">
-        <div className="d-flex align-items-center justify-content-between">
-          Message text <IxButton>Action</IxButton>
-        </div>
-      </IxMessageBar>
+      <div style={{ padding: '0.5rem' }}>
+        <IxMessageBar>Message text</IxMessageBar>
+        <br />
+        <IxMessageBar type="warning">Message text</IxMessageBar>
+        <br />
+        <IxMessageBar type="danger">
+          <div className="d-flex align-items-center justify-content-between">
+            Message text <IxButton>Action</IxButton>
+          </div>
+        </IxMessageBar>
+      </div>
     </>
   );
 };
--- packages/vue-test-app/src/preview-examples/message-bar.vue
+++ packages/vue-test-app/src/preview-examples/message-bar.vue
@@ -12,11 +12,16 @@ import { IxButton, IxMessageBar } from '@siemens/ix-vue';
 </script>
 
 <template>
-  <IxMessageBar>Message text</IxMessageBar>
-  <IxMessageBar type="warning">Message text</IxMessageBar>
-  <IxMessageBar type="danger">
-    <div className="d-flex align-items-center justify-content-between">
-      Message text <IxButton>Action</IxButton>
-    </div>
-  </IxMessageBar>
+  <div style="padding: 0.5rem">
+    <IxMessageBar>Message text</IxMessageBar>
+    <br />
+    <IxMessageBar type="warning">Message text</IxMessageBar
+    <br />
+    <IxMessageBar type="danger">
+      <div className="d-flex align-items-center justify-content-between">
+        Message text <IxButton>Action</IxButton>
+      </div>
+    </IxMessageBar>
+  </div>
+
 </template>
