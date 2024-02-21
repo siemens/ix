63edb87fb22069abc105fc04e88ae56db3d63653
Daniel Leroux <daniel.leroux@siemens.com>
2024-02-21 14:34:45 +0100

    fix: change implementation to vitest

--- packages/react/src/tests/validation-tooltip-null-error/validation-tooltip-null-error.spec.tsx
+++ packages/react/src/tests/validation-tooltip-null-error/validation-tooltip-null-error.spec.tsx
@@ -6,9 +6,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import { render } from '@testing-library/react';
 import React from 'react';
+import { describe, it } from 'vitest';
 import Content from './validation-tooltip-null-error';
 
 describe(`null-error`, () => {
