4d3f895fc61b332f403d1580794e70cbf921868c
Julian Lamplmair <151610373+jul-lam@users.noreply.github.com>
2024-02-26 07:38:52 +0100

    fix(core/panes): expanded and slot error (#1119)

--- packages/core/src/tests/panes/panes.e2e.ts
+++ packages/core/src/tests/panes/panes.e2e.ts
@@ -134,4 +134,15 @@ test.describe('pane', () => {
 
     await expect(page).toHaveScreenshot();
   });
+
+  regressionTest(
+    'slot, correctly toggles while being collapsed through content click',
+    async ({ page }) => {
+      await page.goto('panes/slot');
+      await page.locator('ix-button').first().click();
+      await page.waitForTimeout(1000);
+
+      await expect(page).toHaveScreenshot();
+    }
+  );
 });
