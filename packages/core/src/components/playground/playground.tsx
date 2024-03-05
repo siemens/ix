b25ffd889d33f89c2f1f5dcfb92b16c5c831b572
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-03-05 17:01:55 +0100

    fix(core/menu): changed back playground

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -7,7 +7,7 @@
  * LICENxSE file in the root directory of this source tree.
  */
 
-import {Component, h, Host} from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
 
 /** @internal */
 @Component({
@@ -19,66 +19,6 @@ import {Component, h, Host} from '@stencil/core';
 })
 export class PlaygroundInternal {
   render() {
-    return (
-      <Host>
-        <ix-application>
-          <ix-application-header name="My Application">
-            <div class="placeholder-logo" slot="logo"></div>
-          </ix-application-header>
-          <ix-menu showSettings={false}>
-            <ix-menu-item>Item 1</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-item>Item 2</ix-menu-item>
-            <ix-menu-settings>
-              <ix-menu-settings-item
-                label="Example Setting 1"
-              ></ix-menu-settings-item>
-              <ix-menu-settings-item
-                label="Example Setting 2"
-              ></ix-menu-settings-item>
-            </ix-menu-settings>
-          </ix-menu>
-
-          <ix-content>
-            <ix-content-header
-              slot="header"
-              header-title="My Content Page" >
-            </ix-content-header>
-          </ix-content>
-        </ix-application>
-      </Host>
-    );
+    return <Host></Host>;
   }
 }
