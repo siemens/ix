656cd08fe706e803b5c217f73740b6e68e3d6e3f
Daniel Leroux <daniel.leroux@siemens.com>
2024-03-20 08:46:05 +0100

    ci: add toml

--- scripts/ignore-netlify.js
+++ scripts/ignore-netlify.js
@@ -6,3 +6,11 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+const GIT_COMMIT_MESSAGE = process.env.GIT_COMMIT_MESSAGE;
+
+if (GIT_COMMIT_MESSAGE.includes('[deploy netlify]')) {
+  process.exit(1);
+}
+
+process.exit(0);
