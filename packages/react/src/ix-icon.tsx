d1fd8ede042505f20343167f8937a546f40b449d
Daniel Leroux <daniel.leroux@siemens.com>
2024-04-08 20:25:43 +0200

    fix(react|vue): define webcomponents before usage of utility functions

--- packages/react/src/ix-icon.tsx
+++ packages/react/src/ix-icon.tsx
@@ -9,10 +9,12 @@
 import type { JSX as IxIconsJSX } from '@siemens/ix-icons';
 import { createReactComponent } from './react-component-lib';
 
-import { defineCustomElement as defineIxIcon } from '@siemens/ix-icons/dist/components/ix-icon';
+import { defineCustomElement } from '@siemens/ix-icons/dist/components/ix-icon';
 
 // eslint-disable-next-line no-inline-comments
 export const IxIcon = /*@__PURE__*/ createReactComponent<
   IxIconsJSX.IxIcon,
   HTMLIxIconElement
->('ix-icon', undefined, undefined, defineIxIcon);
+>('ix-icon', undefined, undefined, defineCustomElement);
+
+export const defineIxIcon = defineCustomElement;
--- packages/react/src/toast/toast.ts
+++ packages/react/src/toast/toast.ts
@@ -6,9 +6,11 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { defineCustomElement } from '@siemens/ix/components/ix-toast-container.js';
 
 import { toast, ToastConfig as IxToastConfig } from '@siemens/ix';
 import ReactDOMClient from 'react-dom/client';
+import { defineIxIcon } from 'src/ix-icon';
 
 export type ToastConfig = {
   message: string | React.ReactNode;
@@ -17,6 +19,12 @@ export type ToastConfig = {
 export async function showToast(
   config: Omit<IxToastConfig, 'message'> & ToastConfig
 ) {
+  // Define custom element, otherwise dynamic creation of toast container will fail
+  defineCustomElement();
+
+  // Define ix-icon regarding internal usage
+  defineIxIcon();
+
   if (typeof config.message === 'string') {
     const toastInstance = await toast(config as IxToastConfig);
     return toastInstance;
