3ee7d5f1eea23b82f48b1e0288b4e9abf3edee2b
Daniel Leroux <daniel.leroux@siemens.com>
2024-04-15 15:01:28 +0200

    test: use additional testing lib

--- packages/react/src/tests/toast/toast.spec.tsx
+++ packages/react/src/tests/toast/toast.spec.tsx
@@ -6,10 +6,11 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 import { describe, it } from 'vitest';
 import Content from './toast';
+import { screen } from 'shadow-dom-testing-library';
 
 describe(`toast`, () => {
   it(`basic`, async () => {
@@ -22,24 +23,39 @@ describe(`toast`, () => {
     await customElements.whenDefined('ix-toast-container');
 
     const toast = await screen.findByText('Foobar');
-    const toastShadowRoot = toast.shadowRoot;
 
-    expect(toast).toBeDefined();
-    expect(toastShadowRoot).toBeDefined();
-    expect(toast.innerText).toBe('Foobar');
+    await waitFor(() => {
+      expect(toast).toBeInTheDocument();
+    });
 
-    await customElements.whenDefined('ix-icon');
-
-    const icon = toastShadowRoot?.querySelector(
+    const icon = toast.shadowRoot?.querySelector(
       '.toast-icon ix-icon'
     ) as HTMLIxIconElement;
 
     await waitFor(() => {
-      expect(icon).toBeDefined();
-      expect(icon.shadowRoot).toBeDefined();
+      expect(icon).toBeInTheDocument();
     });
 
-    expect(icon.shadowRoot).toBeDefined();
-    expect(icon.name).toEqual('star');
+    expect(icon?.name).toEqual('star');
+
+    // const toast = await screen.findByText('Foobar');
+    // const toastShadowRoot = toast.shadowRoot;
+
+    // expect(toast).toBeDefined();
+    // expect(toastShadowRoot).toBeDefined();
+    // expect(toast.innerText).toBe('Foobar');
+
+    // await customElements.whenDefined('ix-icon');
+
+    // const icon = toastShadowRoot?.querySelector(
+    //   '.toast-icon ix-icon'
+    // ) as HTMLIxIconElement;
+
+    // await waitFor(() => {
+    //   // expect(icon).toBeInThe
+    //   screen.querby
+    // });
+
+    // expect(icon.name).toEqual('star');
   });
 });
