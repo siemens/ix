7ff2c76a968d3e067a9be5a8152f84f5ef5d4ca8
Daniel Leroux <daniel.leroux@siemens.com>
2024-04-24 11:04:32 +0200

    feat: add standalone putput

---
+++ packages/angular/standalone/src/ix-icon.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  ElementRef,
+  NgZone,
+} from '@angular/core';
+import { defineCustomElements } from '@siemens/ix-icons/loader';
+import { ProxyCmp } from './directives/angular-component-lib/utils';
+
+defineCustomElements();
+
+@ProxyCmp({
+  defineCustomElementFn: defineCustomElements,
+  inputs: ['color', 'size', 'name', 'lazyLoading'],
+})
+@Component({
+  selector: 'ix-icon',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: ['color', 'size', 'name', 'lazyLoading'],
+  standalone: true,
+})
+export class IxIcon {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
+}
