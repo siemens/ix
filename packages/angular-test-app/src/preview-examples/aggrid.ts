1924dc2b0de54e1b269b0d8f2c8158ccd788883e
jul-lam <julian.lamplmair.ext@siemens.com>
2024-04-24 10:02:06 +0200

    fix(angular-test-app): spilt aggrid example into ts and html

--- packages/angular-test-app/src/preview-examples/aggrid.ts
+++ packages/angular-test-app/src/preview-examples/aggrid.ts
@@ -12,14 +12,7 @@ import { GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-example',
-  template: `
-    <ag-grid-angular
-      style="height: 12rem; width: 100%"
-      class="ag-theme-alpine-dark ag-theme-ix"
-      [gridOptions]="gridOptions"
-    >
-    </ag-grid-angular>
-  `,
+  templateUrl: './aggrid.html',
 })
 export default class AGGrid {
   gridOptions = {
