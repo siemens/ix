553424e42779530c94d41ba17dbfea522fc2c9bf
Daniel Leroux <daniel.leroux@siemens.com>
2024-06-26 15:20:01 +0200

    fix: internal property typing

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -783,7 +783,7 @@ export namespace Components {
         /**
           * Get the currently selected date-range.
          */
-        "getCurrentDate": () => Promise<{ from: string | undefined; to: string | undefined; }>;
+        "getCurrentDate": () => Promise<{ from: string; to: string; }>;
         /**
           * Text of date select button
           * @since 2.1.0
@@ -840,7 +840,7 @@ export namespace Components {
           * set styles
          */
         "individual": boolean;
-        "standaloneAppearance": undefined;
+        "standaloneAppearance"?: boolean;
     }
     interface IxDatetimePicker {
         /**
@@ -900,7 +900,7 @@ export namespace Components {
           * @see { this.timeFormat}
           * @since 1.1.0
          */
-        "showTimeReference": undefined;
+        "showTimeReference": any;
         /**
           * Text of date select button
           * @since 1.1.0
@@ -5852,7 +5852,7 @@ declare namespace LocalJSX {
           * set styles
          */
         "individual"?: boolean;
-        "standaloneAppearance"?: undefined;
+        "standaloneAppearance"?: boolean;
     }
     interface IxDatetimePicker {
         /**
@@ -5932,7 +5932,7 @@ declare namespace LocalJSX {
           * @see { this.timeFormat}
           * @since 1.1.0
          */
-        "showTimeReference"?: undefined;
+        "showTimeReference"?: any;
         /**
           * Text of date select button
           * @since 1.1.0
