6fdf32d9ee228ddd7f2117dd37dde304874245e4
matthias <matthias.hader.ext@siemens.com>
2024-07-09 12:39:33 +0200

    fix(core/dropdown): tech depth

--- packages/core/src/components/dropdown-item/dropdown-item.tsx
+++ packages/core/src/components/dropdown-item/dropdown-item.tsx
@@ -30,12 +30,12 @@ export class DropdownItem implements DropdownItemWrapper {
   /**
    * Label of dropdown item
    */
-  @Prop() label: string;
+  @Prop() label?: string;
 
   /**
    * Icon of dropdown item
    */
-  @Prop() icon: string;
+  @Prop() icon?: string;
 
   /**
    * Display hover state
@@ -59,7 +59,7 @@ export class DropdownItem implements DropdownItemWrapper {
   @Prop() suppressChecked = false;
 
   /** @internal */
-  @Event() itemClick: EventEmitter<HTMLIxDropdownItemElement>;
+  @Event() itemClick?: EventEmitter<HTMLIxDropdownItemElement>;
 
   /**
    * Internal usage only
