a361fcf5f0986b42ebfe88febd5f8eb0e8979209
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-07-10 13:33:46 +0200

    fix(core/dropdown): component-doc

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -911,12 +911,12 @@ export namespace Components {
         /**
           * Icon of dropdown item
          */
-        "icon": string;
+        "icon"?: string;
         "isSubMenu": boolean;
         /**
           * Label of dropdown item
          */
-        "label": string;
+        "label"?: string;
         "suppressChecked": boolean;
     }
     /**
