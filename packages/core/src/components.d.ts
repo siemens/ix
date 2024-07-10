0e98af80e3d2d77560f84c3d59e85a8bd87f04ca
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-07-10 13:57:40 +0200

    fix(core/application-header): fixes font size

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -140,7 +140,7 @@ export namespace Components {
         /**
           * Application name
          */
-        "name": string;
+        "name"?: string;
     }
     interface IxApplicationSidebar {
     }
