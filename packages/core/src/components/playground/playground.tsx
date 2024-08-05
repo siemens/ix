394e226583421e1ade6f8a446963b8f65493fcf2
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-05 14:53:38 +0200

    feat(ionic): add ionic support

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -19,6 +19,33 @@ import { Component, h, Host } from '@stencil/core';
 })
 export class PlaygroundInternal {
   render() {
-    return <Host></Host>;
+    return (
+      <Host>
+        <ix-application forceBreakpoint={'md'} breakpoints={['sm', 'md']}>
+          <ix-application-header name="My Application">
+            <placeholder-logo slot="logo"></placeholder-logo>
+
+            <ix-icon-button ghost icon="checkboxes"></ix-icon-button>
+            <ix-icon-button ghost icon="checkboxes"></ix-icon-button>
+            <ix-icon-button ghost icon="checkboxes"></ix-icon-button>
+
+            <ix-dropdown-button variant="secondary" label="Select config" ghost>
+              <ix-dropdown-item label="Config 1"></ix-dropdown-item>
+              <ix-dropdown-item label="Config 2"></ix-dropdown-item>
+              <ix-dropdown-item label="Config 3"></ix-dropdown-item>
+            </ix-dropdown-button>
+
+            <ix-avatar username="John Doe" extra="Administrator">
+              <ix-dropdown-item label="Action 1"></ix-dropdown-item>
+              <ix-dropdown-item label="Action 2"></ix-dropdown-item>
+            </ix-avatar>
+          </ix-application-header>
+          <ix-menu>
+            <ix-menu-item>Test</ix-menu-item>
+          </ix-menu>
+          <ix-content>Test</ix-content>
+        </ix-application>
+      </Host>
+    );
   }
 }
