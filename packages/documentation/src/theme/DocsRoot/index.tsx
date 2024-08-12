e1ef74c6e87cddd3cd8c52fb2e719c15d086c051
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-12 11:48:40 +0200

    fix: link to hash after first time render

---
+++ packages/documentation/src/theme/DocsRoot/index.tsx
@@ -0,0 +1,25 @@
+import React, { useLayoutEffect } from 'react';
+import DocsRoot from '@theme-original/DocsRoot';
+import type DocsRootType from '@theme/DocsRoot';
+import type { WrapperProps } from '@docusaurus/types';
+
+type Props = WrapperProps<typeof DocsRootType>;
+
+export default function DocsRootWrapper(props: Props): JSX.Element {
+  const { history, location } = props;
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      history.push({
+        search: location.search,
+        hash: location.hash,
+      });
+    }, 150);
+  }, []);
+
+  return (
+    <>
+      <DocsRoot {...props} />
+    </>
+  );
+}
