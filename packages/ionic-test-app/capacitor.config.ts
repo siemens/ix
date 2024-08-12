4bea30834dddc64babc871431499ba11e88d2be2
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-12 13:09:27 +0200

    fix: move test app

--- packages/ionic-test-app/capacitor.config.ts
+++ packages/ionic-test-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.siemens.ix-starter',
   appName: 'ix-mobile',
   webDir: 'dist',
 };
