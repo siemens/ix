98fe47fb73bf7a6a4eeabb1226b32469fbf73836
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-13 07:35:01 +0200

    chore: cleanup playground

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -8,7 +8,6 @@
  */
 
 import { Component, h, Host } from '@stencil/core';
-import { showModal } from './../utils/modal';
 
 /** @internal */
 @Component({
@@ -19,72 +18,7 @@ import { showModal } from './../utils/modal';
   scoped: true,
 })
 export class PlaygroundInternal {
-  componentDidLoad() {
-    setTimeout(() => {
-      const div = document.createElement('ix-modal');
-      div.innerHTML = `
-            <ix-modal-header>Create new Event</ix-modal-header>
-            <ix-modal-content>
-              <label>Event name</label>
-              <input
-                style="height: 120vh"
-                type="text"
-              />
-            </ix-modal-content>
-            <ix-modal-footer>
-              <ix-button>Save</ix-button>
-              <ix-button>Cancel</ix-button>
-            </ix-modal-footer>
-      `;
-      showModal({
-        content: div,
-        size: 'full-screen',
-      });
-    }, 100);
-  }
-
   render() {
-    return (
-      <Host>
-        <ix-application>
-          <ix-application-header></ix-application-header>
-          <ix-menu></ix-menu>
-          <div
-            style={{
-              display: 'block',
-              position: 'relative',
-              height: '100%',
-            }}
-          >
-            <ix-button
-              onClick={() => {
-                const div = document.createElement('ix-modal');
-                div.innerHTML = `
-                      <ix-modal-header>Create new Event</ix-modal-header>
-                      <ix-modal-content>
-                        <label>Event name</label>
-                        <input
-                          style="height: 120vh"
-                          type="text"
-                        />
-                      </ix-modal-content>
-                      <ix-modal-footer>
-                        <ix-button>Save</ix-button>
-                        <ix-button>Cancel</ix-button>
-                      </ix-modal-footer>
-                `;
-                showModal({
-                  content: div,
-                  size: 'full-screen',
-                });
-              }}
-            >
-              Test
-            </ix-button>
-          </div>
-          <ix-button slot="bottom">Footer</ix-button>
-        </ix-application>
-      </Host>
-    );
+    return <Host></Host>;
   }
 }
