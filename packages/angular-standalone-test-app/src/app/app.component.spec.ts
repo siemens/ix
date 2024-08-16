51dccfcadb535013e560e7cf3927c80d141f6333
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-16 13:08:01 +0200

    fix: unit test scripts

--- packages/angular-standalone-test-app/src/app/app.component.spec.ts
+++ packages/angular-standalone-test-app/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-standalone-test-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-standalone-test-app');
-  });
 });
