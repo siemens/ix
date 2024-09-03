279c1b2615ea4a3b32367b5518eb09cc36ddaefd
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-09-03 10:09:06 +0200

    fix: formatting

--- packages/angular-test-app/src/main.ts
+++ packages/angular-test-app/src/main.ts
@@ -2,7 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
