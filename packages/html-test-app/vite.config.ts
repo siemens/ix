72458df3a80c7ac33ec38357715fe3d443113345
Daniel Leroux <daniel.leroux@siemens.com>
2024-09-13 16:19:52 +0200

    docs: adapt tsconfig

--- packages/html-test-app/vite.config.ts
+++ packages/html-test-app/vite.config.ts
@@ -10,7 +10,6 @@
 import fs from 'fs';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { copyPreviewStyles } from 'example-styles';
 const __dirname = path.resolve();
 
 const previewPath = path.join(__dirname, 'src', 'preview-examples');
@@ -34,51 +33,6 @@ const additionalTheme = {
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  // copyPreviewStyles(path.join(__dirname, 'src', 'preview-examples', 'styles'));
-
-  // entryPoints.forEach((file) => {
-  //   const htmlFile = fs.readFileSync(path.join(previewPath, file), 'utf-8');
-
-  //   const regex =
-  //     /<link\s*rel=\"stylesheet\"\s*href=['"]\.\/styles\/([^'"]+)['"]\s* \/>/;
-
-  //   fs.writeFileSync(
-  //     path.join(previewPath, file),
-  //     htmlFile.replace(
-  //       regex,
-  //       `<link rel="stylesheet" href="./${file.replace('.html', '.css')}" />`
-  //     )
-  //   );
-  // });
-
-  // entryPoints.forEach((file) => {
-  //   const htmlFile = fs.readFileSync(path.join(previewPath, file), 'utf-8');
-
-  //   const regex =
-  //     /<link\s*rel=\"stylesheet\"\s*href=['"]\.\/styles\/([^'"]+)['"]\s* \/>/;
-  //   const match = htmlFile.match(regex);
-
-  //   if (match) {
-  //     console.log(
-  //       match[1],
-  //       'place parallel to the html file',
-  //       path.join(previewPath, file.replace('.html', '.css'))
-  //     );
-
-  //     const style = fs.readFileSync(
-  //       path.join(__dirname, 'src', 'preview-examples', 'styles', match[1]),
-  //       'utf-8'
-  //     );
-
-  //     fs.writeFileSync(
-  //       path.join(previewPath, file.replace('.html', '.css')),
-  //       style
-  //     );
-  //   }
-  // });
-
-  // throw Error('REMOVE ME');
-
   return {
     base: './',
     root: './src',
