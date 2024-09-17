4978d7d8c5f76a92964cb38b8c7f331bca4eb1a4
Andreas Berliner <andreas.berliner@siemens.com>
2024-09-17 14:54:11 +0200

    fix(core): generate build file and fix typo

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  *
  * This source code is licensed under the MIT license found in the
- * LICENxSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree.
  */
 
 import { Component, Host, h } from '@stencil/core';
