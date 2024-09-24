2edc7e48de4132f3ca95ebfbd6c38332877432fe
Daniel Leroux <daniel.leroux@siemens.com>
2024-09-24 14:55:44 +0200

    ci(strict-check): add pr integration

---
+++ tools/strict-check/check.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import execa from 'execa';
+let tscOutput = '';
+
+const [_, __, changedFiles] = process.argv;
+
+console.log(changedFiles);
+
+try {
+  const tscCommand = await execa('tsc', ['-p', 'tsconfig.sonar.json']);
+  tscOutput = tscCommand.stdout;
+} catch (error) {
+  tscOutput = error.stdout;
+}
+
+const lines = tscOutput.split('\n');
+
+for (const line of lines) {
+  const regexFile = /(.*)(\(\d+,\d+\)):\W(.*)/;
+
+  const match = line.match(regexFile);
+
+  if (match) {
+    const [_, file, position, message] = match;
+    console.log(file);
+    console.log(position);
+    console.log(message);
+    console.log('---');
+  }
+}
