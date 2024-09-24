37cc9b333cb17f39a5d4d4c11a2b0705d87796a7
Daniel Leroux <daniel.leroux@siemens.com>
2024-09-24 16:21:07 +0200

    fix: ci

--- tools/strict-check/check.mjs
+++ tools/strict-check/check.mjs
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 import execa from 'execa';
+import * as core from '@actions/core';
+import mustache from 'mustache';
+
 let tscOutput = '';
 
 const [_, __, ...changedFiles] = process.argv;
-
-console.log(changedFiles);
 
 try {
   const tscCommand = await execa('tsc', ['-p', 'tsconfig.sonar.json']);
@@ -15,7 +16,6 @@ try {
 }
 
 const lines = tscOutput.split('\n');
-
 const output = [];
 
 for (const line of lines) {
@@ -25,13 +25,63 @@ for (const line of lines) {
 
   if (match) {
     const [_, file, position, message] = match;
-    // console.log(file);
-    // console.log(position);
-    // console.log(message);
-    // console.log('---');
 
-    if (changedFiles.includes(file)) {
-      console.log(line);
-    }
+    changedFiles.forEach((changedFile) => {
+      if (changedFile.includes(file)) {
+        output.push({
+          line,
+          file,
+          position,
+          message,
+        });
+      }
+    });
   }
 }
+
+function getCommentBody() {
+  if (output.length === 0) {
+    return '';
+  }
+
+  const comment = `### Report of \`strict\` check
+
+{{#files}}
+<details>
+<summary>{{file}}</summary>
+
+<ul>
+{{#errors}}
+  <li>
+    {{message}}
+  </li>
+{{/errors}}
+</ul>
+
+</details>
+{{/files}}
+
+`;
+
+  const groupedByFile = output.reduce((acc, obj) => {
+    const { file } = obj;
+    if (!acc[file]) {
+      acc[file] = [];
+    }
+    acc[file].push(obj);
+    return acc;
+  }, {});
+
+  const viewData = {
+    files: Object.keys(groupedByFile).map((file) => ({
+      file,
+      errors: groupedByFile[file],
+    })),
+  };
+
+  const rendered = mustache.render(comment, viewData);
+  return rendered;
+}
+
+core.setOutput('body', getCommentBody());
+core.setOutput('has_errors', output.length > 0);
