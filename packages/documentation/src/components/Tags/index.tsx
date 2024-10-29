9135dc7f986ac10d3aece21067f7f553728a65cc
Andreas Berliner <andreas.berliner@siemens.com>
2024-10-29 14:35:28 +0100

    feat(docs): styling for deprecated/legacy tag

--- packages/documentation/src/components/Tags/index.tsx
+++ packages/documentation/src/components/Tags/index.tsx
@@ -1,10 +1,23 @@
-import './TagDeprecated.css';
+import './LegacyTag.css';
+import { IxIcon } from '@siemens/ix-react';
 
-export default function DeprecatedTag(props: { message: string }) {
+export default function LegacyTag(props: { isDeprecated: boolean }) {
   return (
-    <div className="TagDeprecatedContainer">
-      <ix-icon name="open-external" color="color-alarm" size={24}></ix-icon>
-      <div className="TagDeprecatedTitle">Deprecated {props.message}</div>
+    <div
+      className={`TagContainer ${
+        props.isDeprecated ? 'TagDeprecatedContainer' : ''
+      }`}
+    >
+      <IxIcon
+        name="open-external"
+        color={props.isDeprecated ? 'color-alarm' : 'color-primary'}
+        size="24"
+      ></IxIcon>
+      <div
+        className={`Title ${props.isDeprecated ? 'TagDeprecatedTitle' : ''}`}
+      >
+        {props.isDeprecated ? 'Show deprecated version' : 'Show latest version'}
+      </div>
     </div>
   );
 }
