1c1b8044533e8299bac6167ad44e563d32986fc4
Andreas Berliner <andreas.berliner@siemens.com>
2024-10-30 10:00:13 +0100

    feat(docs): deprecated tags component logic and styling

--- packages/documentation/src/components/Tags/index.tsx
+++ packages/documentation/src/components/Tags/index.tsx
@@ -1,26 +1,40 @@
 import './LegacyTag.css';
 import { IxIcon } from '@siemens/ix-react';
 
-export default function LegacyTag(props: {url: string, isDeprecated: boolean }) {
-
+export default function LegacyTag(props: {
+  url: string;
+  hasDeprecatedAncestor: boolean;
+  deprecationVersion?: string;
+}) {
   return (
-    <a
-      href={props.url}
-      target="_blank"
-      className={`TagContainer ${
-        props.isDeprecated ? 'TagDeprecatedContainer' : ''
-      }`}
-    >
-      <IxIcon
-        name="open-external"
-        color={props.isDeprecated ? 'color-alarm' : 'color-primary'}
-        size="16"
-      ></IxIcon>
-      <div
-        className={`Title ${props.isDeprecated ? 'TagDeprecatedTitle' : ''}`}
+    <div className="TagsContainer d-flex gap-2 align-items-center">
+      {!props.hasDeprecatedAncestor && (
+        <span className="Api__Table Docs__Tag Docs__Tag__Deprecated">
+          Deprecated since {props.deprecationVersion}
+        </span>
+      )}
+      <a
+        href={props.url}
+        target="_blank"
+        className={`TagContainer ${
+          props.hasDeprecatedAncestor ? 'TagDeprecatedContainer' : ''
+        }`}
       >
-        {props.isDeprecated ? 'Show deprecated version' : 'Show latest version'}
-      </div>
-    </a>
+        <IxIcon
+          name="open-external"
+          color={props.hasDeprecatedAncestor ? 'color-alarm' : 'color-primary'}
+          size="16"
+        ></IxIcon>
+        <div
+          className={`Title ${
+            props.hasDeprecatedAncestor ? 'TagDeprecatedTitle' : ''
+          }`}
+        >
+          {props.hasDeprecatedAncestor
+            ? 'Show deprecated version'
+            : 'Show latest version'}
+        </div>
+      </a>
+    </div>
   );
 }
