5c2c416a268d52b78ed0e6df56a6798a72c9809f
matthiashader <144090716+matthiashader@users.noreply.github.com>
2024-11-27 09:48:13 +0100

    chore: changelog, build

---
+++ .changeset/afraid-papayas-own.md
@@ -0,0 +1,11 @@
+---
+'eslint-config-ix': major
+'@siemens/ix-angular': major
+'@siemens/ix-echarts': major
+'@siemens/ix-aggrid': major
+'@siemens/ix-react': major
+'@siemens/ix': major
+'@siemens/ix-vue': major
+---
+
+Placeholder
--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -813,7 +813,7 @@ export namespace Components {
         /**
           * Get the currently selected date-range.
          */
-        "getCurrentDate": () => Promise<{ from: string | undefined; to: string | undefined; }>;
+        "getCurrentDate": () => Promise<{ from: string; to: string; }>;
         /**
           * Text of date select button
           * @since 2.1.0
@@ -2723,7 +2723,7 @@ export namespace Components {
         /**
           * Get the current time based on the wanted format
          */
-        "getCurrentTime": () => Promise<string | undefined>;
+        "getCurrentTime": () => Promise<string>;
         /**
           * Show hour input
          */
