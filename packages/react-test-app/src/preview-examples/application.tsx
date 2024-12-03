be58f4d0ff396ee5dce8f84a91b64a7fcbb1de6d
jul-lam <julian.lamplmair.ext@siemens.com>
2024-12-03 11:12:28 +0100

    docs(application): remove avatar and dropdown from basic example

--- packages/react-test-app/src/preview-examples/application.tsx
+++ packages/react-test-app/src/preview-examples/application.tsx
@@ -12,11 +12,8 @@ import './application.css';
 import {
   IxApplication,
   IxApplicationHeader,
-  IxAvatar,
   IxContent,
   IxContentHeader,
-  IxDropdownButton,
-  IxDropdownItem,
   IxMenu,
   IxMenuItem,
 } from '@siemens/ix-react';
@@ -26,18 +23,6 @@ export default () => {
     <IxApplication>
       <IxApplicationHeader name="My Application">
         <div className="placeholder-logo" slot="logo"></div>
-
-        <IxDropdownButton variant="secondary" label="Select config" ghost>
-          <IxDropdownItem label="Config 1"></IxDropdownItem>
-          <IxDropdownItem label="Config 2"></IxDropdownItem>
-          <IxDropdownItem label="Config 3"></IxDropdownItem>
-        </IxDropdownButton>
-
-        <IxAvatar>
-          <IxDropdownItem label="Action 1"></IxDropdownItem>
-          <IxDropdownItem label="Action 2"></IxDropdownItem>
-          <IxDropdownItem label="Action 3"></IxDropdownItem>
-        </IxAvatar>
       </IxApplicationHeader>
 
       <IxMenu>
--- packages/vue-test-app/src/preview-examples/application.vue
+++ packages/vue-test-app/src/preview-examples/application.vue
@@ -11,11 +11,8 @@
 import {
   IxApplication,
   IxApplicationHeader,
-  IxAvatar,
   IxContent,
   IxContentHeader,
-  IxDropdownButton,
-  IxDropdownItem,
   IxMenu,
   IxMenuItem,
 } from '@siemens/ix-vue';
@@ -27,18 +24,6 @@ import {
   <IxApplication>
     <IxApplicationHeader name="My Application">
       <div className="placeholder-logo" slot="logo"></div>
-
-      <IxDropdownButton variant="secondary" label="Select config" ghost>
-        <IxDropdownItem label="Config 1"></IxDropdownItem>
-        <IxDropdownItem label="Config 2"></IxDropdownItem>
-        <IxDropdownItem label="Config 3"></IxDropdownItem>
-      </IxDropdownButton>
-
-      <IxAvatar>
-        <IxDropdownItem label="Action 1"></IxDropdownItem>
-        <IxDropdownItem label="Action 2"></IxDropdownItem>
-        <IxDropdownItem label="Action 3"></IxDropdownItem>
-      </IxAvatar>
     </IxApplicationHeader>
 
     <IxMenu>
