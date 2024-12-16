600e98be84283a9e3184c4ba47521ae1e267f549
jul-lam <julian.lamplmair.ext@siemens.com>
2024-12-16 15:06:27 +0100

    docs(react-test-app): change migration css files

--- packages/react-test-app/src/preview-examples/basic-navigation-migration.tsx
+++ packages/react-test-app/src/preview-examples/basic-navigation-migration.tsx
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import './basic-navigation-migration.css';
+import './basic-navigation-migration.scoped.css';
 
 import {
   IxApplication,
