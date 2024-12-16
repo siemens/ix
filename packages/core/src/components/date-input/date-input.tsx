1fa58c8ce7956d5ba9113126b178d84c32b12d7d
AndreasBerliner <41509230+AndreasBerliner@users.noreply.github.com>
2024-12-16 11:18:35 +0100

    docs(input): add info about slots and examples for input, number-input, date-input (#1616)
    Co-authored-by: Lukas Maurer <lukas.maurer@siemens.com>

--- packages/core/src/components/date-input/date-input.tsx
+++ packages/core/src/components/date-input/date-input.tsx
@@ -42,6 +42,8 @@ export type DateInputValidityState = {
 /**
  * @since 2.6.0
  * @form-ready 2.6.0
+ * @slot start - Element will be displayed at the start of the input
+ * @slot end - Element will be displayed at the end of the input
  */
 @Component({
   tag: 'ix-date-input',
--- packages/core/src/components/input/input.tsx
+++ packages/core/src/components/input/input.tsx
@@ -42,6 +42,8 @@ let inputIds = 0;
 /**
  * @since 2.6.0
  * @form-ready 2.6.0
+ * @slot start - Element will be displayed at the start of the input
+ * @slot end - Element will be displayed at the end of the input
  */
 @Component({
   tag: 'ix-input',
