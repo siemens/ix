9df417047fc304b0b709860055ef0ae3b43625c1
jul-lam <julian.lamplmair.ext@siemens.com>
2025-01-14 15:27:29 +0100

    feat(angular-standalone-test-app): playground for shared testing

--- packages/angular-standalone-test-app/src/playground/playground.ts
+++ packages/angular-standalone-test-app/src/playground/playground.ts
@@ -7,11 +7,69 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import {
+  IxBooleanValueAccessorDirective,
+  IxDateValueAccessorDirective,
+  IxIcon,
+  IxInput,
+  IxRadioValueAccessorDirective,
+  IxSelectValueAccessorDirective,
+  IxTextValueAccessorDirective,
+} from '@siemens/ix-angular/standalone';
+
+export function customRequiredValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    if (control.dirty && !control.value) {
+      return Validators.required(control);
+    }
+    return null;
+  };
+}
+
+/* Zum testen lokal definiert. Auch als export verfügbar */
+export const IxValueAccessorDirectives = [
+  IxBooleanValueAccessorDirective,
+  IxRadioValueAccessorDirective,
+  IxSelectValueAccessorDirective,
+  IxTextValueAccessorDirective,
+  IxDateValueAccessorDirective,
+];
 
 @Component({
-  selector: 'app-example',
+  selector: 'app-playground',
   standalone: true,
+  imports: [ReactiveFormsModule, IxInput, IxIcon],
   templateUrl: './playground.html',
+  styleUrls: ['./playground.css'],
+  hostDirectives: [...IxValueAccessorDirectives],
 })
-export default class Playground {}
+export default class Playground implements OnInit {
+  firstNameLength = 10;
+  lastNameLength = 15;
+
+  form = new FormGroup({
+    first: new FormControl('', [
+      customRequiredValidator(),
+      Validators.maxLength(this.firstNameLength),
+    ]),
+    last: new FormControl('', [
+      customRequiredValidator(),
+      Validators.maxLength(this.lastNameLength),
+    ]),
+  });
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+}
