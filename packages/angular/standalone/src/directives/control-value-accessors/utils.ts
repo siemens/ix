1cecdb8c88241c55a07b78cb8c4b877d50b2802a
jul-lam <julian.lamplmair.ext@siemens.com>
2025-01-14 15:26:27 +0100

    feat(angular): implement utility value accessor array

---
+++ packages/angular/standalone/src/directives/control-value-accessors/utils.ts
@@ -0,0 +1,13 @@
+import { IxBooleanValueAccessorDirective } from './boolean-value-accessor';
+import { IxRadioValueAccessorDirective } from './radio-value-accessor';
+import { IxSelectValueAccessorDirective } from './select-value-accessor';
+import { IxTextValueAccessorDirective } from './text-value-accessor';
+import { IxDateValueAccessorDirective } from './date-value-accessor';
+
+export const IxValueAccessorDirectives = [
+  IxBooleanValueAccessorDirective,
+  IxRadioValueAccessorDirective,
+  IxSelectValueAccessorDirective,
+  IxTextValueAccessorDirective,
+  IxDateValueAccessorDirective,
+];
