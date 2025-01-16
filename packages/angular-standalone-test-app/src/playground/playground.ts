a6c7227c44e7fe73d84ecfc5adb0acccb83c73e0
jul-lam <julian.lamplmair.ext@siemens.com>
2025-01-16 09:03:30 +0100

    fix(angular-standalone-test-app): cleanup playground

--- packages/angular-standalone-test-app/src/playground/playground.ts
+++ packages/angular-standalone-test-app/src/playground/playground.ts
@@ -7,55 +7,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  ValidatorFn,
-  Validators,
-} from '@angular/forms';
-import {
-  IxIcon,
-  IxInput,
-  IxValueAccessorDirectives,
-} from '@siemens/ix-angular/standalone';
-
-export function customRequiredValidator(): ValidatorFn {
-  return (control: AbstractControl) => {
-    if (control.dirty && !control.value) {
-      return Validators.required(control);
-    }
-    return null;
-  };
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   standalone: true,
-  imports: [ReactiveFormsModule, IxInput, IxIcon, ...IxValueAccessorDirectives],
+  imports: [],
   templateUrl: './playground.html',
   styleUrls: ['./playground.css'],
 })
-export default class Playground implements OnInit {
-  firstNameLength = 10;
-  lastNameLength = 15;
-
-  form = new FormGroup({
-    first: new FormControl('', [
-      customRequiredValidator(),
-      Validators.maxLength(this.firstNameLength),
-    ]),
-    last: new FormControl('', [
-      customRequiredValidator(),
-      Validators.maxLength(this.lastNameLength),
-    ]),
-  });
-
-  ngOnInit(): void {
-    this.form.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
-  }
-}
+export default class Playground {}
