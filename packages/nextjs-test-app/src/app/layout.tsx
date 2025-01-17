7935c34d911eece58d69f711bb5cfd15a33fbd1c
Daniel Leroux <daniel.leroux@siemens.com>
2025-01-17 13:40:08 +0100

    chore: remove starter app assets

--- packages/nextjs-test-app/src/app/layout.tsx
+++ packages/nextjs-test-app/src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import '@siemens/ix/dist/siemens-ix/siemens-ix.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
