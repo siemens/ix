856a49ad8a09b2b75226d9b2df6c9ddb3d91751c
Andreas Berliner <andreas.berliner@siemens.com>
2025-01-21 17:10:48 +0100

    docs(pill): add some storybook stories for ix-pill

---
+++ packages/storybook-docs/src/stories/pill.stories.ts
@@ -0,0 +1,70 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import type { Components } from '@siemens/ix/components';
+import type { ArgTypes, Meta, StoryObj } from '@storybook/web-components';
+import { genericRender, makeArgTypes } from './utils/generic-render';
+
+type Element = Components.IxPill & { defaultSlot: string };
+
+const meta = {
+  title: 'Example/Pill',
+  tags: [],
+  render: (args) => genericRender('ix-pill', args),
+  argTypes: makeArgTypes<Partial<ArgTypes<Element>>>('ix-pill', {}),
+  parameters: {},
+} satisfies Meta<Element>;
+
+export default meta;
+type Story = StoryObj<Element>;
+
+export const With_Icon_and_Text: Story = {
+  args: {
+    icon: 'info',
+    defaultSlot: 'Example Text',
+  },
+};
+
+export const With_Icon_only: Story = {
+  args: {
+    icon: 'info',
+  },
+};
+
+export const With_Text_only: Story = {
+  args: {
+    defaultSlot: 'Example Text',
+  },
+};
+
+export const With_Icon_and_Text_Ellipsis: Story = {
+  args: {
+    icon: 'info',
+    defaultSlot: 'Example Text',
+  },
+  render: (args) => {
+    const container = genericRender('ix-pill', args);
+    const ixPill = container.querySelector('ix-pill')!;
+    ixPill.style.width = '5rem';
+
+    return container
+  }
+};
+
+export const With_Text_only_Ellipsis: Story = {
+  args: {
+    defaultSlot: 'Example Text',
+  },
+  render: (args) => {
+    const container = genericRender('ix-pill', args);
+    const ixPill = container.querySelector('ix-pill')!;
+    ixPill.style.width = '5rem';
+
+    return container
+  }
+};
