0b3e27c855415c67839be952fb9649ec15e4c132
Andreas Berliner <andreas.berliner@siemens.com>
2025-01-22 13:44:49 +0100

    docs(pill): move template to separate file for angular test app example

--- packages/angular-test-app/src/preview-examples/pill.ts
+++ packages/angular-test-app/src/preview-examples/pill.ts
@@ -12,33 +12,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-example',
   styleUrls: ['./pill.css'],
-  template: `
-    <ix-pill variant="custom" color="white" background="purple">
-      Label
-    </ix-pill>
-
-    <ix-pill>Label</ix-pill>
-    <ix-pill outline>Label</ix-pill>
-    <ix-pill class="styled">Label</ix-pill>
-
-    <ix-pill icon="star">Label</ix-pill>
-    <ix-pill icon="star"></ix-pill>
-    <ix-pill icon="star" class="styled">Label</ix-pill>
-    <ix-pill outline alignLeft icon="star" class="styled">Label</ix-pill>
-
-    <ix-pill variant="alarm">Label</ix-pill>
-    <ix-pill variant="alarm" outline>Label</ix-pill>
-    <ix-pill variant="alarm" class="styled">Label</ix-pill>
-
-    <ix-pill variant="alarm" icon="star">Label</ix-pill>
-    <ix-pill variant="alarm" icon="star" class="styled">Label</ix-pill>
-    <ix-pill variant="alarm" outline alignLeft icon="star" class="styled">
-      Label
-    </ix-pill>
-    <ix-pill icon="star" style="width: 4rem">Label</ix-pill>
-    <ix-pill outline icon="star" style="width: 4rem">Label</ix-pill>
-    <ix-pill style="width: 3rem">Label</ix-pill>
-    <ix-pill outline style="width: 3rem">Label</ix-pill>
-  `,
+  templateUrl: './pill.html',
 })
 export default class Pill {}
