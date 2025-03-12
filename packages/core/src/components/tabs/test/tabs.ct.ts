caba8c0e9d5dc61e8f2d558d88ccc6992d620a13
Raunak Matai <raunak.matai@siemens.com>
2025-03-12 16:52:14 +0530

    fix(core/tab-item): remove test only

--- packages/core/src/components/tabs/test/tabs.ct.ts
+++ packages/core/src/components/tabs/test/tabs.ct.ts
@@ -24,7 +24,7 @@ test('renders', async ({ mount, page }) => {
   await expect(tab).toHaveClass(/selected/);
 });
 
-test.only('should change tab', async ({ mount, page }) => {
+test('should change tab', async ({ mount, page }) => {
   await mount(`
     <ix-tabs>
       <ix-tab-item>Item 1</ix-tab-item>
@@ -64,7 +64,7 @@ test('should not change tab by tab click event', async ({ mount, page }) => {
   await expect(lastTab).not.toHaveClass(/selected/);
 });
 
-test.only('should not change tab by native click event on prevent default', async ({ mount, page }) => {
+test('should not change tab by native click event on prevent default', async ({ mount, page }) => {
   await mount(`
     <ix-tabs>
       <ix-tab-item onClick={(event) => event.preventDefault()}>Item 1</ix-tab-item>
