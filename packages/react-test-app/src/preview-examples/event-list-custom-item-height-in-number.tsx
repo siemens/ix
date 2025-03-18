33dca967c7381bedc2253c059f1f3a1281145f7c
RamVinayMandal <ram.mondal@siemens.com>
2025-03-18 21:11:19 +0530

    Removed unnecessary react fragment

--- packages/react-test-app/src/preview-examples/event-list-custom-item-height-in-number.tsx
+++ packages/react-test-app/src/preview-examples/event-list-custom-item-height-in-number.tsx
@@ -14,16 +14,14 @@ export default () => {
   const [items] = useState([1, 2, 3]);
 
   return (
-    <>
-      <IxEventList itemHeight={60}>
-        {items.map((item) => (
-          <div key={item}>
-            <IxEventListItem itemColor="color-primary">
-              Text {item}
-            </IxEventListItem>
-          </div>
-        ))}
-      </IxEventList>
-    </>
+    <IxEventList itemHeight={60}>
+      {items.map((item) => (
+        <div key={item}>
+          <IxEventListItem itemColor="color-primary">
+            Text {item}
+          </IxEventListItem>
+        </div>
+      ))}
+    </IxEventList>
   );
 };
