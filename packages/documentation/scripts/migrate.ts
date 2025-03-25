7175c67b125c390651ceca45185f093b2ae31ad8
Daniel Leroux <daniel.leroux@siemens.com>
2025-03-25 17:46:51 +0100

    change all index files

--- packages/documentation/scripts/migrate.ts
+++ packages/documentation/scripts/migrate.ts
@@ -6,17 +6,41 @@ async function run() {
   const code = await glob('**/docs/**/code.mdx');
   const guide = await glob('**/docs/**/guide.mdx');
 
-  console.log(index);
+  // index.forEach(async (file) => {
+  //   const content = await fs.readFile(file, 'utf-8');
 
-  index.forEach(async (file) => {
+  //   const hasSingleTabFeature = content.includes('no_single_tab');
+  //   fs.writeFile(
+  //     file,
+  //     content
+  //       .replace(/import.*$/gms, '')
+  //       .replace(/toc_min_heading_level.*\n/g, '')
+  //       .replace(/toc_max_heading_level.*\n/g, '')
+  //       .replace(/component-tabs.*\n/g, '')
+  //       .replace(
+  //         /doc-type: 'component'/g,
+  //         `doc-type: "${hasSingleTabFeature ? 'banner' : 'tabs'}"`
+  //       )
+  //       .replace(/no_single_tab.*\n/g, '')
+  //   );
+  // });
+
+  code.forEach(async (file) => {
     const content = await fs.readFile(file, 'utf-8');
+    if (content.includes("doc-type: 'tab-item'")) {
+      return;
+    }
+
     console.log(
-      content
-        .replace(/import.*$/gms, '')
-        .replace(/toc_min_heading_level.*\n/g, '')
-        .replace(/toc_max_heading_level.*\n/g, '')
-        .replace(/component-tabs.*\n/g, '')
-        .replace(/doc-type: 'component'/g, 'doc-type: "tabs"')
+      file,
+      [
+        '---',
+        "doc-type: 'tab-item'",
+        '---',
+        content
+          .replace(/#\s(.)/gm, ' $1')
+          .replace(/# Development/g, `# $COMPONENT_NAME$ - Code`),
+      ].join('\n')
     );
   });
 }
