0755d7c6feb136ad04b8a7903297bf05006e724a
RamVinayMandal <ram.mondal@siemens.com>
2025-04-14 21:25:41 +0530

    Removed only from test case

--- packages/core/src/components/input/tests/form-ready.ct.ts
+++ packages/core/src/components/input/tests/form-ready.ct.ts
@@ -87,7 +87,7 @@ test(`form-ready - ix-textarea with initial value`, async ({ mount, page }) => {
   expect(formData).toBe('initial value');
 });
 
-test.only('form-ready - textarea correctly renders character counter with null value', async ({ mount, page }) => {
+test('form-ready - textarea correctly renders character counter with null value', async ({ mount, page }) => {
   await mount(
     `<form><ix-textarea name="my-field-name" max-length="100"></ix-textarea></form>`
   );
