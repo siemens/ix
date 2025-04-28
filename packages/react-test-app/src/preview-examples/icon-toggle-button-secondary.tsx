a7b61c8547e1ea2df11e2c93b6a01a954de68296
Andreas Berliner <andreas.berliner@siemens.com>
2025-04-28 10:59:48 +0200

    docs(icon-toggle-button): remove unnecessary react fragments from react examples

--- packages/react-test-app/src/preview-examples/icon-toggle-button-secondary.tsx
+++ packages/react-test-app/src/preview-examples/icon-toggle-button-secondary.tsx
@@ -14,44 +14,36 @@ import { IxIconToggleButton, IxLayoutGrid, IxRow } from '@siemens/ix-react';
 
 export default () => {
   return (
-    <>
-      <IxLayoutGrid>
-        <IxRow>
-          <IxIconToggleButton icon={iconCheckboxes}></IxIconToggleButton>
-          <IxIconToggleButton
-            pressed
-            icon={iconCheckboxes}
-          ></IxIconToggleButton>
-          <IxIconToggleButton
-            disabled
-            icon={iconCheckboxes}
-          ></IxIconToggleButton>
-          <IxIconToggleButton
-            disabled
-            loading
-            icon={iconCheckboxes}
-          ></IxIconToggleButton>
-        </IxRow>
-        <IxRow>
-          <IxIconToggleButton icon={iconCheckboxes} oval></IxIconToggleButton>
-          <IxIconToggleButton
-            pressed
-            icon={iconCheckboxes}
-            oval
-          ></IxIconToggleButton>
-          <IxIconToggleButton
-            disabled
-            icon={iconCheckboxes}
-            oval
-          ></IxIconToggleButton>
-          <IxIconToggleButton
-            disabled
-            loading
-            icon={iconCheckboxes}
-            oval
-          ></IxIconToggleButton>
-        </IxRow>
-      </IxLayoutGrid>
-    </>
+    <IxLayoutGrid>
+      <IxRow>
+        <IxIconToggleButton icon={iconCheckboxes}></IxIconToggleButton>
+        <IxIconToggleButton pressed icon={iconCheckboxes}></IxIconToggleButton>
+        <IxIconToggleButton disabled icon={iconCheckboxes}></IxIconToggleButton>
+        <IxIconToggleButton
+          disabled
+          loading
+          icon={iconCheckboxes}
+        ></IxIconToggleButton>
+      </IxRow>
+      <IxRow>
+        <IxIconToggleButton icon={iconCheckboxes} oval></IxIconToggleButton>
+        <IxIconToggleButton
+          pressed
+          icon={iconCheckboxes}
+          oval
+        ></IxIconToggleButton>
+        <IxIconToggleButton
+          disabled
+          icon={iconCheckboxes}
+          oval
+        ></IxIconToggleButton>
+        <IxIconToggleButton
+          disabled
+          loading
+          icon={iconCheckboxes}
+          oval
+        ></IxIconToggleButton>
+      </IxRow>
+    </IxLayoutGrid>
   );
 };
