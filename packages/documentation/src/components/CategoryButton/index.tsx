b032ffd666148685e5d5e38c8055a4fe3d2c79ef
Lukas Maurer <lukas.maurer@siemens.com>
2025-04-30 14:03:13 +0200

    docs: make overview categories clickable

---
+++ packages/documentation/src/components/CategoryButton/index.tsx
@@ -0,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { IxButton } from '@siemens/ix-react';
+import styles from './CategoryButton.module.css';
+
+export function CategoryButton(
+  props: React.PropsWithChildren<{
+    title: string;
+    link: string;
+  }>
+) {
+  return (
+    <IxButton
+      ghost
+      className={styles.CategoryButton}
+      onClick={() => {
+        window.location.href = props.link;
+      }}
+    >
+      <div className={styles.CategoryButton__Content}>
+        <div className={styles.CategoryButton__Title}>{props.title}</div>
+        <div className={styles.CategoryButton__Image}>
+          {props.children}
+        </div>
+      </div>
+    </IxButton>
+  );
+}
