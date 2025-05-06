3a3814be041ac02df2ba4225d64b00b5ab5feb09
AndreasBerliner <41509230+AndreasBerliner@users.noreply.github.com>
2025-05-06 17:23:08 +0200

    feat(core/icon-toggle-button): Add oval attribute (#1856)

---
+++ .changeset/sharp-eyes-rescue.md
@@ -0,0 +1,8 @@
+---
+'@siemens/ix-angular': minor
+'@siemens/ix-react': minor
+'@siemens/ix': minor
+'@siemens/ix-vue': minor
+---
+
+Add oval attribute to __ix-icon-toggle-button__.
--- packages/angular-test-app/src/preview-examples/icon-toggle-button-secondary.ts
+++ packages/angular-test-app/src/preview-examples/icon-toggle-button-secondary.ts
@@ -12,14 +12,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-example',
   template: `
-    <ix-icon-toggle-button icon="checkboxes"></ix-icon-toggle-button>
-    <ix-icon-toggle-button pressed icon="checkboxes"></ix-icon-toggle-button>
-    <ix-icon-toggle-button disabled icon="checkboxes"></ix-icon-toggle-button>
-    <ix-icon-toggle-button
-      disabled
-      loading
-      icon="checkboxes"
-    ></ix-icon-toggle-button>
+    <ix-layout-grid>
+      <ix-row>
+        <ix-icon-toggle-button icon="checkboxes"></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          pressed
+          icon="checkboxes"
+        ></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          disabled
+          icon="checkboxes"
+        ></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          disabled
+          loading
+          icon="checkboxes"
+        ></ix-icon-toggle-button>
+      </ix-row>
+      <ix-row>
+        <ix-icon-toggle-button icon="checkboxes" oval></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          pressed
+          icon="checkboxes"
+          oval
+        ></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          disabled
+          icon="checkboxes"
+          oval
+        ></ix-icon-toggle-button>
+        <ix-icon-toggle-button
+          disabled
+          loading
+          icon="checkboxes"
+          oval
+        ></ix-icon-toggle-button>
+      </ix-row>
+    </ix-layout-grid>
   `,
   styleUrls: ['./icon-toggle-button-secondary.css'],
 })
