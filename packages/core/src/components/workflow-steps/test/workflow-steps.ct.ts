8d7d35451529a5e583135364aa5690b4f4b225c4
varun srinivasa <srinivasa-varun.vanamamulai@siemens.com>
2025-05-09 10:22:54 +0530

    updated ct test case

--- packages/core/src/components/workflow-steps/test/workflow-steps.ct.ts
+++ packages/core/src/components/workflow-steps/test/workflow-steps.ct.ts
@@ -6,14 +6,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-/*
- * SPDX-FileCopyrightText: 2024 Siemens AG
- *
- * SPDX-License-Identifier: MIT
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import { expect } from '@playwright/test';
 import { regressionTest } from '@utils/test';
 
@@ -81,3 +73,60 @@ regressionTest('should prevent click navigation', async ({ mount, page }) => {
   await expect(firstStepDiv).toHaveClass(/selected/);
   await expect(lastStepDiv).not.toHaveClass(/selected/);
 });
+
+regressionTest(
+  'should toggle workflow step status between open and error on button click',
+  async ({ mount, page }) => {
+    await mount(`
+    <div>
+    <ix-workflow-steps clickable vertical>
+      <ix-workflow-step id="step1" status='open'>Step 1</ix-workflow-step>
+      <ix-workflow-step status='success'>Step 2</ix-workflow-step>
+      <ix-workflow-step status='error'>Step 3</ix-workflow-step>
+    </ix-workflow-steps>
+
+    <ix-button id="toggle-button">Toggle Step 1</ix-button>
+    </div>
+  `);
+    const workflowSteps = page.locator('ix-workflow-steps');
+    const selectedStep = workflowSteps.locator('ix-workflow-step').nth(0);
+    const selectedDiv = selectedStep.locator('.step');
+
+    await expect(workflowSteps).toHaveClass(/hydrated/);
+    await expect(selectedDiv).toHaveClass(/selected/);
+
+    await page.evaluate(() => {
+      const button = document.getElementById('toggle-button');
+      const step1 = document.getElementById('step1');
+
+      button?.addEventListener('click', () => {
+        const currentStatus = step1?.getAttribute('status');
+        step1?.setAttribute(
+          'status',
+          currentStatus === 'open' ? 'error' : 'open'
+        );
+      });
+    });
+
+    const toggleBtn = page.locator('#toggle-button');
+    const step1 = page.locator('#step1');
+    await toggleBtn.click();
+    await expect(step1).toHaveAttribute('status', 'error');
+
+    const icon2 = page.locator('#step1 ix-icon').nth(1);
+    await expect(icon2).toHaveAttribute(
+      'style',
+      'color: var(--theme-color-alarm);'
+    );
+    await toggleBtn.click();
+    await expect(step1).toHaveAttribute('status', 'open');
+
+    const icon1 = page.locator('#step1 ix-icon').nth(1);
+    await expect(icon1).toHaveAttribute(
+      'style',
+      'color: var(--theme-workflow-step-icon-default--color--selected);'
+    );
+
+    await expect(selectedDiv).toHaveClass(/selected/);
+  }
+);
