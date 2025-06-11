c41551ef865c4a07e6c18c4407f69b0d31a7f03c
Nadim901 <nadim.almas@siemens.com>
2025-06-12 01:25:10 +0530

    Traverse up the DOM

--- packages/angular/common/src/directives/control-value-accessors/radio-value-accessor.ts
+++ packages/angular/common/src/directives/control-value-accessors/radio-value-accessor.ts
@@ -12,6 +12,10 @@ import { ValueAccessor } from './value-accessor';
 
 @Directive()
 export class RadioValueAccessorBaseDirective extends ValueAccessor {
+
+  private readonly MAX_PARENT_LEVELS = 10;
+  private readonly PARENT_TAG = 'ix-radio-group';
+
   constructor(injector: Injector, el: ElementRef) {
     super(injector, el);
   }
@@ -22,13 +26,34 @@ export class RadioValueAccessorBaseDirective extends ValueAccessor {
   }
 
   private handleRadioGroupValueChange(): void {
-    const parentElement = this.elementRef.nativeElement.parentElement;
+    const currentElement = this.elementRef.nativeElement;
+    const parentElement = this.findRadioGroup(currentElement);
+
     if (parentElement) {
-      parentElement.addEventListener('valueChange',
-        (event: CustomEvent) => {
-          this.lastValue = event.detail;
-        });
+      parentElement.addEventListener('valueChange', (event: Event) => {
+        const customEvent = event as CustomEvent;
+        this.lastValue = customEvent.detail;
+      });
+    } else {
+      console.warn('Expected parent element not found');
     }
+  }
+
+  private findRadioGroup(currentElement: HTMLElement | null): HTMLElement | null {
+    let ancestorLevel = 0;
+
+    while (currentElement && ancestorLevel < this.MAX_PARENT_LEVELS) {
+      if (currentElement.tagName.toLowerCase() === this.PARENT_TAG) {
+        return currentElement;
+      }
+      currentElement = currentElement.parentElement;
+      ancestorLevel++;
+    }
+
+    if (ancestorLevel >= this.MAX_PARENT_LEVELS) {
+      console.error(`Radio button must be placed within ix-radio-group element`);
+    }
+    return null;
   }
 
   override writeValue(value: string): void {
