45448b72a8d080644f04e99fc69e402d5b46b1f4
Daniel Leroux <daniel.leroux@siemens.com>
2025-06-27 13:18:33 +0200

    fix vue typings

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,9 +6,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+export * from './public-api';
 export { handlePlatformHelpers, IxConfig } from './setup';
-
 export type * from './components.d.ts';
 
 export type {
