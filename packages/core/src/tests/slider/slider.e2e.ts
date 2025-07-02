5c0043a1efcee8827e2a2e8e6cb0c915bf5ee218
Devdarsh Panth <devdarsh.panth@siemens.com>
2025-07-02 11:21:31 +0530

    Updated snapshots wrt figma

--- packages/core/src/tests/slider/slider.e2e.ts
+++ packages/core/src/tests/slider/slider.e2e.ts
@@ -151,32 +151,6 @@ regressionTest(
   }
 );
 
-regressionTest('should render with error', async ({ page, mount }) => {
-  await mount(`
-    <div id='slider-container'>
-
-      <ix-slider error="Some error message">
-      </ix-slider>
-
-      <ix-slider error="Some error message">
-        <span slot="label-start">500</span>
-        <span slot="label-end">1000</span>
-      </ix-slider>
-
-    </div>
-  `);
-
-  const slider1 = page.locator('ix-slider').nth(0);
-  const slider2 = page.locator('ix-slider').nth(1);
-
-  await expect(slider1).toHaveClass(/hydrated/);
-  await expect(slider2).toHaveClass(/hydrated/);
-
-  expect(
-    await page.locator('#slider-container').screenshot()
-  ).toMatchSnapshot();
-});
-
 regressionTest('should render all slider variants', async ({ page, mount }) => {
   await mount(`
     <style>
