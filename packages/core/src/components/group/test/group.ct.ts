a256189cfd7183120f42a4d8b632e07198dfdbd9
varun srinivasa <srinivasa-varun.vanamamulai@siemens.com>
2025-07-07 13:44:16 +0530

    removed uncessary test case

--- packages/core/src/components/group/test/group.ct.ts
+++ packages/core/src/components/group/test/group.ct.ts
@@ -150,38 +150,3 @@ regressionTest(
     await expect(group).not.toHaveAttribute('selected');
   }
 );
-
-regressionTest(
-  'group context menu presence with dropdown slot',
-  async ({ mount, page }) => {
-    await mount(`
-      <ix-group>
-        <ix-dropdown slot="dropdown">
-          <ix-dropdown-item label="Item 1"></ix-dropdown-item>
-          <ix-dropdown-item label="Item 2"></ix-dropdown-item>
-        </ix-dropdown>
-        <ix-group-item text="Example text 1"></ix-group-item>
-        <ix-group-item text="Example text 2"></ix-group-item>
-        <ix-group-item text="Example text 3"></ix-group-item>
-      </ix-group>
-    `);
-
-    const contextMenu = page.locator('ix-group-context-menu');
-    await expect(contextMenu).toHaveCount(1);
-  }
-);
-
-regressionTest(
-  'absence of group context menu when no dropdown slot',
-  async ({ mount, page }) => {
-    await mount(`
-      <ix-group>
-        <ix-group-item text="Example text 1"></ix-group-item>
-        <ix-group-item text="Example text 2"></ix-group-item>
-        <ix-group-item text="Example text 3"></ix-group-item>
-      </ix-group>
-    `);
-    const contextMenu = page.locator('ix-group-context-menu');
-    await expect(contextMenu).toHaveCount(0);
-  }
-);
