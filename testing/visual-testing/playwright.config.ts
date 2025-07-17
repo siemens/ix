2914a2818cc6d77fe8a078a4edb1ffa3fd3664f3
Daniel Leroux <daniel.leroux@siemens.com>
2025-07-17 17:25:45 +0200

    optimize test setup

--- testing/visual-testing/playwright.config.ts
+++ testing/visual-testing/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 10,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
