fa07090546ebec3e2a8b89bda5871d20af26b338
Daniel Leroux <daniel.leroux@siemens.com>
2025-07-24 08:16:27 +0200

    revert: pnpm-lock.yaml update

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -103,7 +103,6 @@ export const config: Config = {
       stencilPackageName: corePackageName,
       outDir: '../react/src',
       excludeComponents: ['ix-tree', 'ix-tree-item', 'ix-icon'],
-      clientModule: '@siemens/ix',
       hydrateModule: '@siemens/ix/hydrate',
       serializeShadowRoot: { scoped: [], default: 'declarative-shadow-dom' },
     }),
