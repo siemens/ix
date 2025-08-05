2a3050105108851827c40f97c574f1080b890af8
Nadim901 <nadim.almas@siemens.com>
2025-08-05 22:25:37 +0530

    removed comments

--- packages/core/src/components/utils/menu-tabs/menu-tabs-utils.ts
+++ packages/core/src/components/utils/menu-tabs/menu-tabs-utils.ts
@@ -23,24 +23,22 @@ function getItems(
 }
 
 export function setTab(context: MenuSettings | MenuAbout, label: string) {
-
   const { defaultPrevented } = context.tabChange.emit(label);
 
   if (defaultPrevented) {
     return;
   }
 
-  // Set flag to indicate this is an internal change
   context.isInternalTabChange = true;
-
   context.activeTabLabel = label;
-
-  // Reset flag after the change
   context.isInternalTabChange = false;
   syncTabDisplay(context, label);
 }
 
-export function syncTabDisplay(context: MenuSettings | MenuAbout, label: string) {
+export function syncTabDisplay(
+  context: MenuSettings | MenuAbout,
+  label: string
+) {
   context.items.forEach((i) => {
     i.style.display = 'none';
     if (i.label === label) {
