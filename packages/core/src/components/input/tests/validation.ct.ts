dea5425fc6792251c36e34c4ae522f2fbab6b5f5
lzeiml <lukas.zeiml@siemens.com>
2025-08-13 13:37:28 +0200

    fix tests

--- packages/core/src/components/input/tests/validation.ct.ts
+++ packages/core/src/components/input/tests/validation.ct.ts
@@ -76,7 +76,7 @@ test.describe('validation', () => {
       mount,
       page,
     }) => {
-      await mount('<ix-number-input></ix-number-input>');
+      await mount('<ix-number-input step="0.1"></ix-number-input>');
 
       const ixInput = page.locator('ix-number-input');
       const shadowDomInput = ixInput.locator('input');
@@ -84,16 +84,19 @@ test.describe('validation', () => {
       // Test positive scientific notation
       await shadowDomInput.fill('1E6');
       await shadowDomInput.blur();
+      await expect(shadowDomInput).toHaveValue('1000000');
       await expect(ixInput).not.toHaveClass(/ix-invalid/);
 
       // Test negative scientific notation
-      await shadowDomInput.fill('1E-6');
+      await shadowDomInput.fill('1E-1');
       await shadowDomInput.blur();
+      await expect(shadowDomInput).toHaveValue('0.1');
       await expect(ixInput).not.toHaveClass(/ix-invalid/);
 
       // Test lowercase scientific notation
       await shadowDomInput.fill('2.5e3');
       await shadowDomInput.blur();
+      await expect(shadowDomInput).toHaveValue('2500');
       await expect(ixInput).not.toHaveClass(/ix-invalid/);
     });
 
@@ -111,19 +114,15 @@ test.describe('validation', () => {
       await expect(ixInput).not.toHaveClass(/ix-invalid/);
     });
 
-    test('number input should validate invalid text input', async ({
-      mount,
-      page,
-    }) => {
-      await mount('<ix-number-input required></ix-number-input>');
+    test('number input should reject letter input', async ({ mount, page }) => {
+      await mount('<ix-number-input required value="1"></ix-number-input>');
 
       const ixInput = page.locator('ix-number-input');
       const shadowDomInput = ixInput.locator('input');
 
-      await shadowDomInput.fill('abc');
-      await shadowDomInput.blur();
+      await shadowDomInput.pressSequentially('abc');
 
-      await expect(ixInput).toHaveClass(/ix-invalid/);
+      await expect(shadowDomInput).toHaveValue('1');
     });
 
     test('increment by step', async ({ mount, page }) => {
