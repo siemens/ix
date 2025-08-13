51e22bdf27092f0e7d6bf9c514baa0651a4ec731
1307-Dev <devdarsh.panth@siemens.com>
2025-08-13 13:53:37 +0530

    Revert "Added prettier"
    This reverts commit c1ff7fa12b34fca76c5b4af7123a89ddb8160a82.

--- packages/angular-test-app/src/preview-examples/toast-custom.ts
+++ packages/angular-test-app/src/preview-examples/toast-custom.ts
@@ -27,19 +27,15 @@ import { ToastService } from '@siemens/ix-angular';
 
     <ng-template #customToastAction let-toast>
       <div>
-        <ix-button ghost icon="undo" (click)="toast.close('Action')"
-          >Undo</ix-button
-        >
+        <ix-button ghost icon="undo" (click)="toast.close('Action')">Undo</ix-button>
       </div>
     </ng-template>
   `,
-  styles: [
-    `
-      ix-button {
-        margin-right: 0.5rem;
-      }
-    `,
-  ],
+  styles: [`
+    ix-button {
+      margin-right: 0.5rem;
+    }
+  `]
 })
 export default class ToastCustom {
   @ViewChild('customToastMessage', { read: TemplateRef })
