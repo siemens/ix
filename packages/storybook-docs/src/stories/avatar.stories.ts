f704c0ae355db7cc21ccd4a2c580102700f471d5
Daniel Leroux <daniel.leroux@siemens.com>
2025-08-19 11:17:35 +0200

    cleanup

--- packages/storybook-docs/src/stories/avatar.stories.ts
+++ packages/storybook-docs/src/stories/avatar.stories.ts
@@ -9,7 +9,6 @@
 import type { Components } from '@siemens/ix/components';
 import type { ArgTypes, Meta, StoryObj } from '@storybook/web-components';
 import { genericRender, makeArgTypes } from './utils/generic-render';
-import { html } from 'lit';
 
 type Element = Components.IxAvatar;
 
