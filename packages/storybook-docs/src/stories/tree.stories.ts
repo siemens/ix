2fda7af71571092dab350efddb70c17bb5822784
raghavendra5577 <raghavendra.bangi.ext@siemens.com>
2025-08-25 11:43:33 +0530

    changes

--- packages/storybook-docs/src/stories/tree.stories.ts
+++ packages/storybook-docs/src/stories/tree.stories.ts
@@ -1,93 +1,142 @@
 import { html } from 'lit';
-import type { StoryObj } from '@storybook/web-components';
+import type { Components } from '@siemens/ix/components';
+import type { ArgTypes, Meta, StoryObj } from '@storybook/web-components';
+import { makeArgTypes } from './utils/generic-render';
+
+type Element = Components.IxTree;
 
 const meta = {
   title: 'Example/Tree',
-  parameters: {
-    controls: { expanded: true },
-  },
-  argTypes: {
-    expandOn: {
-      control: { type: 'select' },
-      options: ['chevron', 'node'],
-    },
-    selectable: { control: 'boolean' },
-  },
-};
-export default meta;
-
-type Story = StoryObj;
-
-export const SampleTree: Story = {
+  tags: [],
   render: (args) => html`
     <ix-tree
-      expand-on=${args.expandOn}
-      ?selectable=${args.selectable}
-    >
-      <ix-tree-item label="sample" expanded>
-        <ix-tree-item label="sample1"></ix-tree-item>
-        <ix-tree-item label="sample2"></ix-tree-item>
-      </ix-tree-item>
-    </ix-tree>
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
   `,
-  args: {
-    expandOn: 'chevron',
-    selectable: false,
+  argTypes: makeArgTypes<Partial<ArgTypes<Element>>>('ix-tree', {
+    model: { control: false },
+    root: { control: false },
+    context: { control: false },
+  }),
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/r2nqdNNXXZtPmWuVjIlM1Q/iX-Components---Brand-Dark?node-id=721-9472&m=dev',
+    },
+    controls: { expanded: true },
   },
+} satisfies Meta<Element>;
+
+export default meta;
+type Story = StoryObj<Element>;
+
+const defaultModel = {
+  root: {
+    id: 'root',
+    data: { name: '' },
+    hasChildren: true,
+    children: ['sample'],
+    expanded: false
+  },
+  sample: {
+    id: 'sample',
+    data: { name: 'Sample' },
+    hasChildren: true,
+    children: ['sample-child-1', 'sample-child-2'],
+    expanded: false,
+  },
+  'sample-child-1': {
+    id: 'sample-child-1',
+    data: { name: 'Sample Child 1' },
+    hasChildren: false,
+    children: [],
+  },
+  'sample-child-2': {
+    id: 'sample-child-2',
+    data: { name: 'Sample Child 2' },
+    hasChildren: false,
+    children: [],
+  },
+};
+
+export const Default: Story = {
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: false, isSelected: false } },
+  },
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
+    `,
 };
 
 export const ExpandOnChevronClick: Story = {
-  name: 'Expand only when chevron is clicked',
-  render: () => html`
-    <ix-tree expand-on="chevron">
-      <ix-tree-item label="Parent">
-        <ix-tree-item label="Child 1"></ix-tree-item>
-        <ix-tree-item label="Child 2"></ix-tree-item>
-      </ix-tree-item>
-    </ix-tree>
-    <p>Parent expands only when chevron is clicked.</p>
+  name: 'Parent node expands only when chevron is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: false, isSelected: false } },
+  },
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
   `,
 };
 
 export const ExpandOnNodeClick: Story = {
-  name: 'Expand when entire node is clicked',
-  render: () => html`
-    <ix-tree expand-on="node">
-      <ix-tree-item label="Parent">
-        <ix-tree-item label="Child 1"></ix-tree-item>
-        <ix-tree-item label="Child 2"></ix-tree-item>
-      </ix-tree-item>
-    </ix-tree>
-    <p>Parent expands when the entire node is clicked.</p>
+  name: 'Parent node expands when the entire node is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: false, isSelected: false } },
+  },
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
   `,
 };
 
 export const SelectableExpandOnChevron: Story = {
-  name: 'Selectable parent, expand only on chevron',
-  render: () => html`
-    <ix-tree expand-on="chevron" selectable>
-      <ix-tree-item label="Parent" selectable>
-        <ix-tree-item label="Child 1"></ix-tree-item>
-        <ix-tree-item label="Child 2"></ix-tree-item>
-      </ix-tree-item>
-    </ix-tree>
-    <p>
-      Selectable parent nodes expand only when chevron is clicked and change to selected when entire node is clicked.
-    </p>
+  name: 'Selectable parent nodes expand only when chevron is clicked and change to selected when entire node is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: false, isSelected: false } },
+
+  },
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
   `,
 };
 
 export const SelectableExpandOnNode: Story = {
-  name: 'Selectable parent, expand and select on node click',
-  render: () => html`
-    <ix-tree expand-on="node" selectable>
-      <ix-tree-item label="Parent" selectable>
-        <ix-tree-item label="Child 1"></ix-tree-item>
-        <ix-tree-item label="Child 2"></ix-tree-item>
-      </ix-tree-item>
-    </ix-tree>
-    <p>
-      Selectable parent nodes both expand and change to selected when entire node is clicked.
-    </p>
+  name: 'Selectable parent nodes both expand and change to selected when entire node is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: true, isSelected: true } },
+  },
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+    ></ix-tree>
   `,
 };
