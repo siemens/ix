06d4062d1ff6546c9057168143b8eae462ef0fe4
Daniel Leroux <daniel.leroux@siemens.com>
2025-09-05 12:22:11 +0200

    add first mapping draft

--- packages/aggrid/src/index.ts
+++ packages/aggrid/src/index.ts
@@ -6,38 +6,22 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import type AgGridCommunity from 'ag-grid-community';
 // import filterIcon from './filter-icon.style.css';
 import checkboxStyles from './checkbox.style.css';
+import { aggridIxThemeParams } from './aggrid-ix-theme-params.ts';
 
 type AgGridModule = typeof AgGridCommunity;
 
 const useIxTheme = async (
   importModule: () => Promise<AgGridModule> | AgGridModule
 ) => {
-  const { createTheme, createPart, themeAlpine } = await importModule();
+  const { createPart, themeAlpine } = await importModule();
   const base = themeAlpine.withParams({
-    backgroundColor: 'var(--theme-color-1)',
-    foregroundColor: 'var(--theme-color-std-text)',
-    headerBackgroundColor: 'transparent',
-    oddRowBackgroundColor: 'transparent',
-    rowHoverColor: 'var(--theme-table-data-row--background--hover)',
-    selectedRowBackgroundColor:
-      'var(--theme-table-data-row--background--selected)',
-
-    rangeSelectionBackgroundColor:
-      'var(--theme-table-data-row--background--selected)',
-    rangeSelectionBorderColor: 'var(--theme-input--border-color--focus)',
-    headerColumnResizeHandleColor:
-      'var(--theme-table-header-splitter--background)',
-    headerColumnResizeHandleHeight: '100%',
-    headerColumnResizeHandleWidth: '1px',
-
-    inputFocusShadow: 'none',
+    ...aggridIxThemeParams,
   });
 
-  function checkboxPart() {
+  function modifyParts() {
     return base.withoutPart('checkboxStyle').withPart(
       createPart({
         feature: 'checkboxStyle',
@@ -47,7 +31,7 @@ const useIxTheme = async (
     );
   }
 
-  return checkboxPart();
+  return modifyParts();
 };
 
 export { useIxTheme };
