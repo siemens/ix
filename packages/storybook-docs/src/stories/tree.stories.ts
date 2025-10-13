94d4b040ee751841f7ba8bae525fb7f534169d56
B-LakshmiPriya <lakshmi.priya-b@siemens.com>
2025-10-13 11:55:45 +0530

    create story book for exploring click behaviour

---
+++ packages/storybook-docs/src/stories/tree.stories.ts
@@ -0,0 +1,113 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { html } from 'lit';
+import type { Components } from '@siemens/ix/components';
+import type { ArgTypes, Meta, StoryObj } from '@storybook/web-components';
+import { makeArgTypes } from './utils/generic-render';
+
+type Element = Components.IxTree;
+
+const meta = {
+  title: 'Example/Tree',
+  tags: [],
+  render: (args) => html`
+    <ix-tree
+      .model=${args.model}
+      .root=${args.root}
+      .context=${args.context}
+      .toggleOnItemClick=${args.toggleOnItemClick}
+    ></ix-tree>
+  `,
+  argTypes: makeArgTypes<Partial<ArgTypes<Element>>>('ix-tree', {
+    model: { control: false },
+    root: { control: false },
+    context: { control: false },
+    toggleOnItemClick: {
+      control: 'boolean',
+      description: 'Enable to toggle items by click on the item',
+    },
+  }),
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/r2nqdNNXXZtPmWuVjIlM1Q/iX-Components---Brand-Dark?node-id=721-9472&m=dev',
+    },
+    controls: { expanded: true },
+  },
+} satisfies Meta<Element>;
+
+export default meta;
+type Story = StoryObj<Element>;
+
+const defaultModel = {
+  root: {
+    id: 'root',
+    data: { name: '' },
+    hasChildren: true,
+    children: ['sample'],
+  },
+  sample: {
+    id: 'sample',
+    data: { name: 'Sample' },
+    hasChildren: true,
+    children: ['sample-child-1', 'sample-child-2'],
+  },
+  'sample-child-1': {
+    id: 'sample-child-1',
+    data: { name: 'Sample Child 1' },
+    hasChildren: false,
+    children: [],
+  },
+  'sample-child-2': {
+    id: 'sample-child-2',
+    data: { name: 'Sample Child 2' },
+    hasChildren: false,
+    children: [],
+  },
+};
+
+export const DefaultCollapsedOnStart: Story = {
+  name: 'Default',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: false, isSelected: false } },
+    toggleOnItemClick: false,
+  },
+};
+
+export const ExpandOnChevronClick: Story = {
+  name: 'Parent node expands only when chevron is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: true, isSelected: false } },
+    toggleOnItemClick: false,
+  },
+};
+
+export const ExpandOnNodeClick: Story = {
+  name: 'Parent node expands when the entire node is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: true, isSelected: false } },
+    toggleOnItemClick: true,
+  },
+};
+
+export const SelectableExpandOnNode: Story = {
+  name: 'Selectable parent nodes both expand and change to selected when entire node is clicked',
+  args: {
+    root: 'root',
+    model: defaultModel,
+    context: { sample: { isExpanded: true, isSelected: true } },
+    toggleOnItemClick: true,
+  },
+};
