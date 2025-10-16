d05228ab156853ca05f64dcfff9d2645906c9d0b
Lukas Maurer <lukas.maurer@siemens.com>
2025-10-16 13:10:56 +0200

    docs(core/dropdown-button): remove unused property value form examples

--- packages/angular-test-app/src/preview-examples/dropdown-button.ts
+++ packages/angular-test-app/src/preview-examples/dropdown-button.ts
@@ -15,16 +15,16 @@ import { Component } from '@angular/core';
   template: `
     <div class="example">
       <ix-dropdown-button label="Dropdown" icon="checkboxes">
-        <ix-dropdown-item label="Item 1" value="1"></ix-dropdown-item>
-        <ix-dropdown-item label="Item 2" value="2"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 1"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 2"></ix-dropdown-item>
       </ix-dropdown-button>
       <ix-dropdown-button variant="subtle-primary" label="Dropdown" icon="checkboxes">
-        <ix-dropdown-item label="Item 1" value="1"></ix-dropdown-item>
-        <ix-dropdown-item label="Item 2" value="2"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 1"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 2"></ix-dropdown-item>
       </ix-dropdown-button>
       <ix-dropdown-button variant="tertiary" label="Dropdown" icon="checkboxes">
-        <ix-dropdown-item label="Item 1" value="1"></ix-dropdown-item>
-        <ix-dropdown-item label="Item 2" value="2"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 1"></ix-dropdown-item>
+        <ix-dropdown-item label="Item 2"></ix-dropdown-item>
       </ix-dropdown-button>
       <ix-dropdown-button label="Dropdown" disabled icon="checkboxes">
       </ix-dropdown-button>
