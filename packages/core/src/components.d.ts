22808f4b7ec5fb49cd533ac7af2b805008f428df
@gayatri.kalyani <gayatri.kalyani@siemens.com>
2025-10-25 13:19:47 +0530

    corrected typo jsDocs in toggle button tsx code

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -3892,7 +3892,7 @@ export namespace Components {
     }
     interface IxToggleButton {
         /**
-          * ARIA label for the button Will be set as aria-label on the nested HTML button element
+          * ARIA label that will be set on the native HTML button element
           * @since 3.2.0
          */
         "ariaLabelButton"?: string;
@@ -9990,7 +9990,7 @@ declare namespace LocalJSX {
     }
     interface IxToggleButton {
         /**
-          * ARIA label for the button Will be set as aria-label on the nested HTML button element
+          * ARIA label that will be set on the native HTML button element
           * @since 3.2.0
          */
         "ariaLabelButton"?: string;
