e6ea314fe6418e698146698cbd0a0c70d56836ca
Daniel Leroux <daniel.leroux@siemens.com>
2025-12-02 16:09:19 +0100

    dont overwrite disposable

--- packages/core/src/components/dropdown/dropdown.tsx
+++ packages/core/src/components/dropdown/dropdown.tsx
@@ -344,7 +344,7 @@ export class Dropdown implements ComponentInterface, DropdownInterface {
       this.onTriggerClick
     );
 
-    this.disposeClickListener = addDisposableEventListener(
+    this.disposeKeyListener = addDisposableEventListener(
       this.triggerElement,
       'keydown',
       this.onTriggerKeydown as EventListener
