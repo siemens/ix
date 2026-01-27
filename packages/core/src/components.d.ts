8a1de8b1c0142d8c3a162b740e40d68301330768
lakshmi-priya-b <lakshmi.priya-b@siemens.com>
2026-01-27 14:41:43 +0530

    fix(core/menu-avatar): remove title attribute and add tooltipText property to add ix-tooltip (#2337)
    Co-authored-by: Lukas Maurer <lukas.maurer@siemens.com>

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -2312,6 +2312,11 @@ export namespace Components {
     }
     interface IxMenuAvatar {
         /**
+          * aria-label for the tooltip
+          * @since 4.3.0.
+         */
+        "ariaLabelTooltip"?: string;
+        /**
           * Second line of text
          */
         "bottom"?: string;
@@ -2339,6 +2344,11 @@ export namespace Components {
           * Display the initials of the user. Will be overwritten by image
          */
         "initials"?: string;
+        /**
+          * Tooltip text to display on hover. If not set, the 'top' property (user name) will be used as the default tooltip text.
+          * @since 4.3.0.
+         */
+        "tooltipText"?: string;
         /**
           * First line of text
          */
@@ -8330,6 +8340,11 @@ declare namespace LocalJSX {
     }
     interface IxMenuAvatar {
         /**
+          * aria-label for the tooltip
+          * @since 4.3.0.
+         */
+        "ariaLabelTooltip"?: string;
+        /**
           * Second line of text
          */
         "bottom"?: string;
@@ -8361,6 +8376,11 @@ declare namespace LocalJSX {
           * Logout click
          */
         "onLogoutClick"?: (event: IxMenuAvatarCustomEvent<any>) => void;
+        /**
+          * Tooltip text to display on hover. If not set, the 'top' property (user name) will be used as the default tooltip text.
+          * @since 4.3.0.
+         */
+        "tooltipText"?: string;
         /**
           * First line of text
          */
