aff9e2b121cd148b2ff76925a878e2617cdb9696
Gayatri <gayatri.kalyani@siemens.com>
2026-02-16 20:09:29 +0530

    docs(core/buttons): Updated button examples according to new variant naming (#2379)
    Co-authored-by: Lukas Maurer <lukas.maurer@siemens.com>

--- packages/angular-test-app/src/preview-examples/button-secondary.ts
+++ packages/angular-test-app/src/preview-examples/button-secondary.ts
@@ -12,8 +12,8 @@ import { Component } from '@angular/core';
 @Component({
   standalone: false,
   selector: 'app-example',
-  template: ` <ix-button variant="subtle-primary"> Button </ix-button>
-    <ix-button variant="subtle-primary" disabled> Button </ix-button>`,
+  template: ` <ix-button variant="secondary"> Button </ix-button>
+    <ix-button variant="secondary" disabled> Button </ix-button>`,
   styleUrls: ['./button-secondary.css'],
 })
 export default class Buttons {}
