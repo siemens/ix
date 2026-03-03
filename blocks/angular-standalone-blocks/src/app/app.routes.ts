445b7551145da5390db98e510c1e88f5a6e07e26
Daniel Leroux <daniel.leroux@siemens.com>
2026-03-03 16:52:53 +0100

    add first blocks to registry

--- blocks/angular-standalone-blocks/src/app/app.routes.ts
+++ blocks/angular-standalone-blocks/src/app/app.routes.ts
@@ -4,23 +4,11 @@ export const routes: Routes = [
   {
     path: 'change-password',
     loadComponent: () =>
-      import('../routes/change-password/change-password').then(
-        (m) => m.ChangePassword,
-      ),
-  },
-  {
-    path: 'example-01',
-    loadComponent: () => import('../routes/example-01/example').then((m) => m.Example),
+      import('../routes/change-password/change-password').then((m) => m.ChangePassword),
   },
   {
     path: 'login-overlay',
     loadComponent: () =>
-      import('../routes/login-overlay/login-overlay').then(
-        (m) => m.LoginOverlay,
-      ),
-  },
-  {
-    path: 'upload-01',
-    loadComponent: () => import('../routes/upload-01/upload-files').then((m) => m.UploadFiles),
+      import('../routes/login-overlay/login-overlay').then((m) => m.LoginOverlay),
   },
 ];
--- blocks/react-blocks/src/main.tsx
+++ blocks/react-blocks/src/main.tsx
@@ -4,9 +4,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import '@siemens/ix/dist/siemens-ix/siemens-ix.css';
 
 import Index from './routes/index';
-import ExamplePattern from './routes/example-01/index';
 import LoginOverlay from './routes/login-overlay/login-overlay';
-import UploadFiles from './routes/upload-01/upload-files';
 import ChangePassword from './routes/change-password/change-password';
 
 createRoot(document.getElementById('root')!).render(
@@ -14,9 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/example-01" element={<ExamplePattern />} />
         <Route path="/login-overlay" element={<LoginOverlay />} />
-        <Route path="/upload-01" element={<UploadFiles />} />
         <Route path="/change-password" element={<ChangePassword />} />
       </Routes>
     </HashRouter>
