87e026f40d7e318b4b0682b23ba69a654cd79afa
Daniel Leroux <daniel.leroux@siemens.com>
2026-03-25 15:11:00 +0100

    add password criteria for angular

--- blocks/angular-standalone-blocks/src/app/app.routes.ts
+++ blocks/angular-standalone-blocks/src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
       import('../routes/change-password/change-password').then((m) => m.ChangePassword),
   },
   {
+    path: 'password-criteria',
+    loadComponent: () =>
+      import('../routes/password-criteria/password-criteria').then((m) => m.PasswordCriteria),
+  },
+  {
     path: 'login-overlay',
     loadComponent: () =>
       import('../routes/login-overlay/login-overlay').then((m) => m.LoginOverlay),
