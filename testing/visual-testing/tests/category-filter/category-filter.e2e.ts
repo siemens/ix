149dd589bf01e8dac8a86c11d6f3669658528ee5
Lukas Zeiml <lukas.zeiml@siemens.com>
2026-03-30 09:40:32 +0200

    tests

--- testing/visual-testing/tests/category-filter/category-filter.e2e.ts
+++ testing/visual-testing/tests/category-filter/category-filter.e2e.ts
@@ -35,7 +35,7 @@ regressionTest.describe('category-filter', () => {
   regressionTest('category options', async ({ page }) => {
     await page.goto('category-filter/categories');
     await page.locator('input').first().click();
-    await page.locator('.category-item').first().click();
+    await page.locator('ix-dropdown-item').first().click();
 
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
   });
@@ -43,7 +43,7 @@ regressionTest.describe('category-filter', () => {
   regressionTest('static operator', async ({ page }) => {
     await page.goto('category-filter/static-operator');
     await page.locator('input').first().click();
-    await page.locator('.category-item').first().click();
+    await page.locator('ix-dropdown-item').first().click();
 
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
   });
@@ -57,7 +57,10 @@ regressionTest.describe('category-filter', () => {
       const vendorButton = page.getByRole('button', { name: 'Vendor' });
       await vendorButton.click();
 
-      const filterButton = page.getByRole('button', { name: '= Apple' });
+      const operandButton = page.getByRole('button', { name: 'equals (=)' });
+      await operandButton.click();
+
+      const filterButton = page.getByRole('button', { name: 'Apple' });
       await filterButton.click();
 
       await expect(page).toHaveScreenshot();
