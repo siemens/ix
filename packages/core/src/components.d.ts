83daef41ec7d52299b990eb579e4123925bef91e
Alex Kaduk <o.kaduk@gmail.com>
2026-06-07 20:27:03 +0300

    fix(popover): address review comments IX-3942

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -4247,17 +4247,17 @@ export namespace Components {
         /**
           * Interval for hour selection.
           * @since 3.2.0
-          * @default HOUR_INTERVAL_DEFAULT
+          * @default 1
          */
         "hourInterval": number;
         /**
           * Text of the time confirm button.
-          * @default CONFIRM_BUTTON_DEFAULT
+          * @default 'Confirm'
          */
         "i18nConfirmTime": string;
         /**
           * Text for the top header.
-          * @default HEADER_DEFAULT
+          * @default 'Time'
          */
         "i18nHeader": string;
         /**
@@ -4288,7 +4288,7 @@ export namespace Components {
         /**
           * Interval for millisecond selection.
           * @since 3.2.0
-          * @default MILLISECOND_INTERVAL_DEFAULT
+          * @default 100
          */
         "millisecondInterval": number;
         /**
@@ -4299,13 +4299,13 @@ export namespace Components {
         /**
           * Interval for minute selection.
           * @since 3.2.0
-          * @default MINUTE_INTERVAL_DEFAULT
+          * @default 1
          */
         "minuteInterval": number;
         /**
           * Interval for second selection.
           * @since 3.2.0
-          * @default SECOND_INTERVAL_DEFAULT
+          * @default 1
          */
         "secondInterval": number;
         /**
@@ -11045,17 +11045,17 @@ declare namespace LocalJSX {
         /**
           * Interval for hour selection.
           * @since 3.2.0
-          * @default HOUR_INTERVAL_DEFAULT
+          * @default 1
          */
         "hourInterval"?: number;
         /**
           * Text of the time confirm button.
-          * @default CONFIRM_BUTTON_DEFAULT
+          * @default 'Confirm'
          */
         "i18nConfirmTime"?: string;
         /**
           * Text for the top header.
-          * @default HEADER_DEFAULT
+          * @default 'Time'
          */
         "i18nHeader"?: string;
         /**
@@ -11086,7 +11086,7 @@ declare namespace LocalJSX {
         /**
           * Interval for millisecond selection.
           * @since 3.2.0
-          * @default MILLISECOND_INTERVAL_DEFAULT
+          * @default 100
          */
         "millisecondInterval"?: number;
         /**
@@ -11097,7 +11097,7 @@ declare namespace LocalJSX {
         /**
           * Interval for minute selection.
           * @since 3.2.0
-          * @default MINUTE_INTERVAL_DEFAULT
+          * @default 1
          */
         "minuteInterval"?: number;
         /**
@@ -11111,7 +11111,7 @@ declare namespace LocalJSX {
         /**
           * Interval for second selection.
           * @since 3.2.0
-          * @default SECOND_INTERVAL_DEFAULT
+          * @default 1
          */
         "secondInterval"?: number;
         /**
