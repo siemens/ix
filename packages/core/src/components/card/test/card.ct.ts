0f6c969a94f3223202e6d694aacfa3d1df9bb8f9
lakshmi-priya-b <lakshmi.priya-b@siemens.com>
2026-06-24 12:18:42 +0000

    test(card):add test case for keyboard interaction of cards

--- packages/core/src/components/card/test/card.ct.ts
+++ packages/core/src/components/card/test/card.ct.ts
@@ -47,4 +47,38 @@ regressionTest.describe('accessibility', () => {
     const accessibilityScanResults = await makeAxeBuilder().analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
+  regressionTest(
+    'non passive cards are interactive and can be activated with keyboard',
+    async ({ mount, page }) => {
+      await mount(`
+      <ix-card>
+        <ix-card-content>Interactive Card</ix-card-content>
+      </ix-card>
+    `);
+
+      const card = page.locator('ix-card');
+
+      await page.evaluate(() => {
+        const el = document.querySelector('ix-card') as HTMLIxCardElement & {
+          __clickCount: number;
+        };
+        el.__clickCount = 0;
+        el.addEventListener('click', () => {
+          el.__clickCount += 1;
+        });
+      });
+
+      await card.press('Enter');
+      await card.press(' ');
+
+      const clickCount = await page.evaluate(() => {
+        const el = document.querySelector('ix-card') as HTMLIxCardElement & {
+          __clickCount: number;
+        };
+        return el.__clickCount;
+      });
+
+      expect(clickCount).toBe(2);
+    }
+  );
 });
